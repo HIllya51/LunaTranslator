2757398e55dad533fb8339c46901c7767ded60f8
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-10-02 19:32:18 +0800

    1

--- LunaTranslator/textsource/textractor.py
+++ LunaTranslator/textsource/textractor.py
@@ -21,32 +21,41 @@ class textractor(basetext):
         self.hookselectdialog=hookselectdialog
         self.p = QProcess()    
         self.p.readyReadStandardOutput.connect(self.handle_stdout)  
+        self.p.finished.connect(self.cleanup)
         self.p.start(f"./files/Textractor/x{arch}/TextractorCLI.exe")
         self.pid=pid
         self.pname=pname
         self.arch=arch
         self.notarch='86' if arch=='64' else '64'
-        self.attach(self.pid)
+        
+        
         self.textfilter=''
         self.autostart=False
         self.HookCode=None 
         self.userinserthookcode=[]
         self.runonce_line=''
+         
+        self.attach(self.pid)
         super( ).__init__(textgetmethod) 
         self.re=re.compile('\[([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):(.*):(.*@.*)\] (.*)\n')
+    def cleanup(self):
+        print('end')
     def inserthook(self,hookcode):
-        self.p.write( QByteArray((f'{hookcode} -P{self.pid}\r\r').encode(encoding='utf-16-le'))) 
+        print(f'{hookcode} -P{self.pid}\r\n')
+        #self.p.write( QByteArray((f'{hookcode} -P{self.pid}\r\n').encode(encoding='utf-16-le'))) 
         
     def exit(self):
+         
         self.p.write( QByteArray((f'11\r\n').encode(encoding='utf-16-le'))) 
     def attach(self,pid): 
-         
+        print(f'attach -P{pid}\r\n')
         self.p.write( QByteArray((f'attach -P{pid}\r\n').encode(encoding='utf-16-le'))) 
     def detach(self,pid):
         self.p.write( QByteArray((f'detach -P{pid}\r\n').encode(encoding='utf-16-le'))) 
     def handle_stdout(self): 
         data = self.p.readAllStandardOutput()
         stdout = bytes(data).decode("utf16",errors='ignore') 
+        #print(stdout)
         reres=self.re.findall(stdout) #re.findall('\[([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):(.*):(.*@.*)\] (.*)\n',stdout)
         for ares in reres:
             thread_handle,thread_tp_processId, thread_tp_addr, thread_tp_ctx, thread_tp_ctx2, thread_name,HookCode,output =ares
@@ -62,7 +71,7 @@ class textractor(basetext):
             if key==self.selectedhook:
                 self.newline.put(output) 
                 self.runonce_line=output
-            
+             
             if key not in self.hookdatacollecter:
                 if self.autostart:
                     if self.autostarthookcode==HookCode:
