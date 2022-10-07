3fb74b08f3392e5e40bd6e6122feb79d7bbc8cff
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-10-07 16:55:22 +0800

    1

--- LunaTranslator/translator/dreye.py
+++ LunaTranslator/translator/dreye.py
@@ -1,6 +1,7 @@
  
 import re
 import time
+from traceback import print_exc
 from urllib.parse import quote 
 from translator.basetranslator import basetrans
 import platform 
@@ -20,7 +21,7 @@ class TS(basetrans):
             } 
         }
      
-    def x64(self,content):
+    def x64(self,content): 
         try:
             configfile=globalconfig['fanyi'][self.typename]['argsfile']
             
@@ -34,27 +35,46 @@ class TS(basetrans):
                 return ''
             else:
                 path = js['args']['路径'] 
-             
+            path=os.path.join(path,'DreyeMT\\SDK\\bin')
+            path2=os.path.join(path,"TransCOM.dll")
             ress=''
             for line in content.split('\n'):
                 if len(line)==0:
                     continue
                 if ress!='':
-                    ress+='\n'
-                            
+                    ress+='\n' 
                 st=subprocess.STARTUPINFO()
                 st.dwFlags=subprocess.STARTF_USESHOWWINDOW
                 st.wShowWindow=subprocess.SW_HIDE 
-                p=subprocess.Popen(r'./files/x64_x86_dll/dreye.exe "'+path+'"  "'+line+'"',stderr=subprocess.PIPE, stdout=subprocess.PIPE,startupinfo=st)
+
+
+                exec=r'./files/x64_x86_dll/dreyec.exe "'+path+'"  "'+path2+'" '
+                
+                linebyte=bytes(line,encoding='shift-jis')
+                for b in linebyte:
+                    exec+=str(b)+' '
+                p=subprocess.Popen(exec,stdin=subprocess.PIPE,  stdout=subprocess.PIPE,startupinfo=st,cwd=path)
                 l=p.stdout.readline()   
                 #print(l)
-                 
-                ress+=str(l,encoding='gbk',errors='ignore').replace('\r','').replace('\n','').replace(chr(0),'')
-                 
+                
+                res=str(l,encoding='gbk',errors='ignore').replace('\r','').replace('\n','') 
+                #print(res)
+                x=res.split(' ')
+                bs=[]
+                #print(x)
+                for _x in x:
+                    try:
+                        bs.append(int(_x))
+                    except:
+                        break
+                #print(bs)
+                ress+=str(bytes(bs),encoding='gbk')
+
                 #print(1,ress,2)
             #ress=ress.replace('Translation(TaskNo = 1) is OK. (remainder threads = 0)\r\n','')
             return ress 
         except:
+            print_exc()
             return ''
     def translate(self,content): 
         return self.x64(content)
