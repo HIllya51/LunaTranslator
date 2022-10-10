b280d83e85dcb785be874e54793d64a8953d1507
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-10-10 14:48:37 +0800

    0

--- LunaTranslator/translator/baidu.py
+++ LunaTranslator/translator/baidu.py
@@ -32,23 +32,23 @@ class TS(basetrans):
         }
 
         self.session = requests.Session()
-        self.session.headers.update(self.headers)
         #self.session1= httpx.Client(headers=self.headers)
         index_url = 'https://fanyi.baidu.com/'
-        
-        self.session.get(url=index_url, headers=self.headers, proxies=  {'http': None,'https': None},timeout = globalconfig['translatortimeout'] )  #session自动生成cookie
-        response_index = self.session.get(url=index_url , proxies=  {'http': None,'https': None}  )
-        
-        self.token = re.findall(r"token: '([0-9a-z]+)'", response_index.text)[0]
-        self.gtk = re.findall(r'gtk = "(.*?)"', response_index.text)[0]
-        dirname, filename = os.path.split(os.path.abspath(__file__))
-        #print(self.gtk)
-        with open(os.path.join('./files/scripts/baidufanyi_encrypt.js'), 'r', encoding='utf-8') as f:
-            baidu_js = f.read()
-        #self.jsrun=execjs.get('local_node').compile(baidu_js)
-        self.ctx=  EvalJs()
-        self.ctx.execute(baidu_js)
-    
+        try:
+            self.session.get(url=index_url, headers=self.headers, proxies=  {'http': None,'https': None},timeout = globalconfig['translatortimeout'] )  #session自动生成cookie
+            response_index = self.session.get(url=index_url ,headers=self.headers, proxies=  {'http': None,'https': None}  )
+            
+            self.token = re.findall(r"token: '([0-9a-z]+)'", response_index.text)[0]
+            self.gtk = re.findall(r'gtk = "(.*?)"', response_index.text)[0]
+            dirname, filename = os.path.split(os.path.abspath(__file__))
+            #print(self.gtk)
+            with open(os.path.join('./files/scripts/baidufanyi_encrypt.js'), 'r', encoding='utf-8') as f:
+                baidu_js = f.read()
+            #self.jsrun=execjs.get('local_node').compile(baidu_js)
+            self.ctx=  EvalJs()
+            self.ctx.execute(baidu_js)
+        except:
+            print_exc()
     
     
     # 获取今天任意时刻的时间戳
@@ -62,44 +62,67 @@ class TS(basetrans):
         return str(int(tsp*1000))
  
     def translate(self,query): 
-        #sign =self.jsrun.call('e', query, self.gtk)
-        sign=self.ctx.e(query,self.gtk)
-        translate_url = 'https://fanyi.baidu.com/#'+self.srclang()+'/zh/%s' % ( parse.quote(query))
-        #acs_token = self.jsrun.call('ascToken', translate_url)
-
-        acs_token=self.today_anytime_tsp(15,0,9)+ self.ctx.ascToken(translate_url)
-        data = {
-            'from': self.srclang(),
-            'to': 'zh',
-            'query': query,
-            'transtype': 'realtime',
-            'simple_means_flag': '3',
-            'sign': sign,
-            'token': self.token,
-            'domain': 'common'
-        }
-         
-        self.session.headers["Acs-Token"]=acs_token
-        translate_api = 'https://fanyi.baidu.com/v2transapi'
-        
-        response = self.session.post(url=translate_api,   data=data,timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None})
         try:
+        #sign =self.jsrun.call('e', query, self.gtk)
+            sign=self.ctx.e(query,self.gtk)
+            translate_url = 'https://fanyi.baidu.com/#'+self.srclang()+'/zh/%s' % ( parse.quote(query))
+            #acs_token = self.jsrun.call('ascToken', translate_url)
+
+            acs_token=self.today_anytime_tsp(15,0,9)+ self.ctx.ascToken(translate_url)
+            data = {
+                'from': self.srclang(),
+                'to': 'zh',
+                'query': query,
+                'transtype': 'realtime',
+                'simple_means_flag': '3',
+                'sign': sign,
+                'token': self.token,
+                'domain': 'common'
+            }
+            
+            
+            headers = {
+                'Accept': '*/*',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Origin': 'https://fanyi.baidu.com',
+                'Referer': 'https://fanyi.baidu.com/',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+            }
+            headers["Acs-Token"]=acs_token
+            translate_api = 'https://fanyi.baidu.com/v2transapi'
+                    
+        
+            response = self.session.post(url=translate_api,headers=headers,   data=data,timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None})
             result = response.json()['trans_result']['data'][0]['dst']
+            params = {
+                'req': 'check',
+                'fanyi_src': query,
+                'direction': self.srclang()+'2zh',
+                '_': int(time.time()*1000),
+            }
+
+            #response = self.session.get('https://fanyi.baidu.com/pcnewcollection', params=params,timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None} )
+            return result
         except:
             self.inittranslator()
-            print(response.json())
-            #print_exc()
-             
+            print_exc()
+            try :
+                print(response.json())
+            except:
+                pass
             result='出错了'
-        params = {
-            'req': 'check',
-            'fanyi_src': query,
-            'direction': self.srclang()+'2zh',
-            '_': int(time.time()*1000),
-        }
-
-        response = self.session.get('https://fanyi.baidu.com/pcnewcollection', params=params,timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None} )
-        return result
+            return result
+        
+        
     def show(self,res):
         print('百度','\033[0;32;47m',res,'\033[0m',flush=True)
      
--- LunaTranslator/translator/bing.py
+++ LunaTranslator/translator/bing.py
@@ -12,7 +12,7 @@ class TS(basetrans):
         return ['ja','en'][globalconfig['srclang']]
     def inittranslator(self):  
         self.ss=requests.session()
-        self.ss.get('https://cn.bing.com/translator/',headers = { 
+        headers = { 
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
                 'cache-control': 'no-cache',
@@ -33,9 +33,9 @@ class TS(basetrans):
                 'sec-fetch-user': '?1',
                 'upgrade-insecure-requests': '1',
                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53',
-            }, proxies=  {'http': None,'https': None}).text
+            }
          
-        response = self.ss.get('https://cn.bing.com/translator/' ,timeout = globalconfig['translatortimeout'], proxies=  {'http': None,'https': None})
+        response = self.ss.get('https://cn.bing.com/translator/' ,headers=headers, timeout = globalconfig['translatortimeout'], proxies=  {'http': None,'https': None})
         text=response.text
         
         res=re.search('var params_RichTranslateHelper = \[([0-9]+),"(([0-9a-zA-Z]|-|_)+)"',text).group()
@@ -54,42 +54,41 @@ class TS(basetrans):
     def show(self,res):
         print('必应','\033[0;31;47m',res,'\033[0m',flush=True)
     def translate(self,content): 
-         
-        data = '&fromLang='++self.srclang()+'&text='+quote(content)+'&to=zh-Hans&token='+self.token+'&key='+self.key
-        self.iid_i+=1
-                
-        headers = {
-            'authority': 'cn.bing.com',
-            'accept': '*/*',
-            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'cache-control': 'no-cache',
-            'content-type': 'application/x-www-form-urlencoded',
-               'origin': 'https://cn.bing.com',
-            'pragma': 'no-cache',
-            'referer': 'https://cn.bing.com/translator/',
-            'sec-ch-ua': '"Microsoft Edge";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            'sec-ch-ua-arch': '"x86"',
-            'sec-ch-ua-bitness': '"64"',
-            'sec-ch-ua-full-version': '"105.0.1343.53"',
-            'sec-ch-ua-full-version-list': '"Microsoft Edge";v="105.0.1343.53", "Not)A;Brand";v="8.0.0.0", "Chromium";v="105.0.5195.127"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-ch-ua-platform-version': '"10.0.0"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53',
-            'x-edge-shopping-flag': '1',
-        }
         try:
+            data = '&fromLang='+self.srclang()+'&text='+quote(content)+'&to=zh-Hans&token='+self.token+'&key='+self.key
+            self.iid_i+=1
+                    
+            headers = {
+                'authority': 'cn.bing.com',
+                'accept': '*/*',
+                'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded',
+                'origin': 'https://cn.bing.com',
+                'pragma': 'no-cache',
+                'referer': 'https://cn.bing.com/translator/',
+                'sec-ch-ua': '"Microsoft Edge";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+                'sec-ch-ua-arch': '"x86"',
+                'sec-ch-ua-bitness': '"64"',
+                'sec-ch-ua-full-version': '"105.0.1343.53"',
+                'sec-ch-ua-full-version-list': '"Microsoft Edge";v="105.0.1343.53", "Not)A;Brand";v="8.0.0.0", "Chromium";v="105.0.5195.127"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-ch-ua-platform-version': '"10.0.0"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53',
+                'x-edge-shopping-flag': '1',
+            }
+        
             response = self.ss.post('https://cn.bing.com/ttranslatev3?isVertical=1&&IG='+self.IG+'&IID='+self.iid+'.'+str(self.iid_i) ,headers=headers, data=data, proxies=  {'http': None,'https': None},timeout = globalconfig['translatortimeout'])
-            js=response.json()
-            print(self.ss.cookies)
+            js=response.json() 
             ch=js[0]['translations'][0]['text']
             
             return ch
         except:
-            print(js)
+            
             self.inittranslator()
             print_exc()
             return '出错了'
