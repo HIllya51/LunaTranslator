33f3f3d5c76de5482a600801f90d714e52b037d2
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-10-19 21:59:37 +0800

    0

--- LunaTranslator/otherocr/windowsocr.py
+++ LunaTranslator/otherocr/windowsocr.py
@@ -6,12 +6,22 @@ def ocr(imgfile):
         st.dwFlags=subprocess.STARTF_USESHOWWINDOW
         st.wShowWindow=subprocess.SW_HIDE
 
-        p=subprocess.Popen('./files/Windows.Media.Ocr.Cli.exe -l ja '+imgfile,stdout=subprocess.PIPE,startupinfo=st)
+        p=subprocess.Popen('./files/WinOCR.exe ja '+imgfile,stdout=subprocess.PIPE,startupinfo=st)
         x=p.stdout.readlines()
          
         xx=''
-        for _ in x:
-            xx+=str(_,encoding='gbk',errors='ignore').replace(' ','').replace('\r','').replace('\n','')
+        ress={}
+        ress2=[]
+        try:
+            for _ in x:
+                line=str(_,encoding='gbk',errors='ignore').replace('\r','').replace('\n','')
+                
+                ress[ (line.split(' ')[1])]=int(line.split(' ')[0])
+                ress2.append( (line.split(' ')[1]))
+            ress2.sort(key= lambda x:ress[x])
+            xx=''.join(ress2)
+        except:
+            xx=''
         return xx
 if __name__=="__main__":
     baiduocr('1.jpg')
