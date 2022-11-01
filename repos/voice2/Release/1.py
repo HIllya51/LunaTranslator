66bf8b53c479dbfeec391974a508d74cc2af4d0d
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-11-01 23:06:34 +0800

    1

--- repos/voice2/Release/1.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-p=subprocess.Popen("voice2.exe",stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-p.stdin.write("おはよう\r\n".encode("utf-16-le"))
-while True:
-    x=p.stdout.readline()
-    if x!=b'':
-       print(x)
-import winsound
-winsound.PlaySound(open('1.wav','rb'))
