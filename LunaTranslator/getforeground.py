e2e825a6d6fed383bca3a933d27e95458bcdedf3
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-11-07 17:40:08 +0800

    0

---
+++ LunaTranslator/getforeground.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,QPushButton
+
+from PyQt5.QtCore import QCoreApplication ,Qt ,QProcess
+ 
+import win32api,win32gui,win32process,win32con,win32print,win32ui
+import ctypes
+import json
+from ctypes import c_int32,c_char_p,c_uint32,c_float
+import time,threading,multiprocessing
+import os
+import win32,win32event
+from queue import Queue
+from utils.magpie import  callmagpie1
+
+while True:
+    m_hWnd=win32gui.GetForegroundWindow()
+    pid=win32process.GetWindowThreadProcessId(m_hWnd) [1]
+    title = win32gui.GetWindowText(m_hWnd)
+    print(m_hWnd)
+    print(win32gui.GetClassName(m_hWnd))  
+    time.sleep(1)
