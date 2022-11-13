7102017906671b12cb81e38dd2502b1b4a566609
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-11-13 22:02:21 +0800

    0

--- LunaTranslator/gui/settingpage_xianshishezhi.py
+++ LunaTranslator/gui/settingpage_xianshishezhi.py
@@ -62,19 +62,19 @@ def setTabThree(self) :
         except:
                 magpiemethod=[]#['Lanczos','FSR','FSRCNNX','ACNet','Anime4K','CRT-Geom','Integer Scale 2x','Integer Scale 3x']
         
-        def singleton_():
-                if globalconfig['allowmulti']==False:
-                        with open('files/single.txt','w') as ff:
-                                pass
-                else:
-                        try:
-                                os.remove('files/single.txt')
-                        except:
-                                pass
-        def singleton(x):
-                globalconfig['allowmulti']=x
-                singleton_()
-        singleton_()
+        # def singleton_():
+        #         if globalconfig['allowmulti']==False:
+        #                 with open('files/single.txt','w') as ff:
+        #                         pass
+        #         else:
+        #                 try:
+        #                         os.remove('files/single.txt')
+        #                 except:
+        #                         pass
+        # def singleton(x):
+        #         globalconfig['allowmulti']=x
+        #         singleton_()
+        # singleton_()
         magpiesettingdialog=[
                 {'t':'switch','d':globalconfig['magpieflags'],'k':'Is3DMode','l':'3D游戏模式'},
                 {'t':'switch','d':globalconfig['magpieflags'],'k':'DisableWindowResizing','l':'缩放时禁用窗口大小调整'},
@@ -116,7 +116,7 @@ def setTabThree(self) :
                 [('翻译结果繁简体显示',6),(self.getsimplecombobox(['大陆简体','马新简体','台灣正體','香港繁體','简体','繁體'],globalconfig,'fanjian'),4)],
                 [('翻译窗口顺时针旋转(重启生效)',6),(self.getsimplecombobox(['0','90','180','270'],globalconfig,'rotation'),4)],
                 [('翻译器显示语言(重启生效)',6),(self.getsimplecombobox(_TRL([ '简体中文','繁体中文','英文']),globalconfig,'languageuse'),4)],
-                [('允许多开软件(重启生效)',6),self.getsimpleswitch(globalconfig,'allowmulti',callback=singleton)],
+                # [('允许多开软件(重启生效)',6),self.getsimpleswitch(globalconfig,'allowmulti',callback=singleton)],
                
         ] 
         self.yitiaolong("显示设置",buttongrid) 
