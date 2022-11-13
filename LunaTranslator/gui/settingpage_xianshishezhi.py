663c6b760f778825281d1c8623381f6498ec6696
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-11-13 20:15:42 +0800

    0

--- LunaTranslator/gui/settingpage_xianshishezhi.py
+++ LunaTranslator/gui/settingpage_xianshishezhi.py
@@ -67,7 +67,10 @@ def setTabThree(self) :
                         with open('files/single.txt','w') as ff:
                                 pass
                 else:
-                        os.remove('files/single.txt')
+                        try:
+                                os.remove('files/single.txt')
+                        except:
+                                pass
         magpiesettingdialog=[
                 {'t':'switch','d':globalconfig['magpieflags'],'k':'Is3DMode','l':'3D游戏模式'},
                 {'t':'switch','d':globalconfig['magpieflags'],'k':'DisableWindowResizing','l':'缩放时禁用窗口大小调整'},
