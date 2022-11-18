796ff9d297d832ece3a823496f9c9621a95ab939
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-11-18 20:40:53 +0800

    0

--- LunaTranslator/translator/basetranslator.py
+++ LunaTranslator/translator/basetranslator.py
@@ -88,10 +88,10 @@ class basetrans:
                     self.inittranslator()
                 except:
                     print_exc()
-                res='' 
+                res=None 
                 
             
-            if res is not None and res!=''   and self.queue.empty() and contentraw==self.newline:
+            if res is not None  and self.queue.empty() and contentraw==self.newline:
                 self.show(contentraw,(self.typename,res,mp))
 
     
