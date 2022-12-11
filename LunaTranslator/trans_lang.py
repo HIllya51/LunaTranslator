e27a779ad3e33a9b434e75ecac4c708810b512b7
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-12-12 00:39:10 +0800

    1

--- LunaTranslator/trans_lang.py
+++ LunaTranslator/trans_lang.py
@@ -1,30 +1,41 @@
 import sys,time
 sys.path.append('./')
 from translator.baiduapi import TS
-
 class TS1(TS):
-    @property
-    def srclang(self):
-        return 'zh'
-    @property
-    def tgtlang(self):
-        return 'fra'
+            
+            @property
+            def srclang(self):
+                return 'zh'
+            @property
+            def tgtlang(self):
+                return self.xxxxx
+            @tgtlang.setter
+            def tgtlang(self,_):
+                self.xxxxx=_
 if __name__=='__main__':
-    TS1.settypename('baiduapi')
-    a=TS1()
+    
     import os,json
     
     f='简体中文.json'
     with open('./files/lang/'+f,'r',encoding='utf8')  as ff:
         js=ff.read()
         js=json.loads(js)
-    with open('./files/lang/Русский язык.json','r',encoding='utf8')  as ff:
-         
-        jsen=json.loads(ff.read())
-    for k in jsen:
+    
+    xxx={'Русский язык':'ru','English':'en',
+    "Español":"spa","한국어":"kor","Français":"fra" ,"繁體中文":"cht"}
+    for kk in xxx:
+        with open(f'./files/lang/{kk}.json','r',encoding='utf8')  as ff:
+            
+            jsen=json.loads(ff.read())
         
-        if k not in js or  js[k]=='':
-            js[k]=k#a.translate(k)
-            print(k,js[k]) 
-            with open('./files/lang/'+f,'w',encoding='utf8')  as ff:
-                ff.write( json.dumps(js,ensure_ascii=False,sort_keys=False, indent=4))
+        TS1.settypename('baiduapi')
+
+        a=TS1()
+        a.tgtlang=xxx[kk] 
+        for k in js:
+            
+            if k not in jsen or  jsen[k]=='':
+                jsen[k]= a.translate(k)
+                print(k,jsen[k]) 
+                with open(f'./files/lang/{kk}.json','w',encoding='utf8')  as ff:
+                    ff.write( json.dumps(jsen,ensure_ascii=False,sort_keys=False, indent=4))
