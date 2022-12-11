be0a48baf0b8cee035c0266a66374b6d5a483978
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2022-12-11 11:46:27 +0800

    0

--- fix/LunaTranslator/translator/baidu.py
+++ fix/LunaTranslator/translator/baidu.py
@@ -4,174 +4,176 @@ from traceback import print_exc
 import requests
 from urllib import parse 
 import os
-import re ,random
+import re 
 from translator.basetranslator import basetrans 
-from js2py import EvalJs
-import urllib
+from js2py import EvalJs  
 from utils.config import globalconfig
 import time
-class Tse:
-    def __init__(self):
-        self.author = 'Ulion.Tse'
-        self.begin_time = time.time()
-        self.default_session_seconds = 1.5e3
-        self.transform_en_translator_pool = ('Itranslate', 'Lingvanex',)
-        self.auto_pool = ('auto', 'auto-detect',)
-        self.zh_pool = ('zh', 'zh-CN', 'zh-CHS', 'zh-Hans', 'zh-Hans_CN', 'cn', 'chi',)
- 
-    @staticmethod
-    def get_headers(host_url: str,
-                    if_api: bool = False,
-                    if_referer_for_host: bool = True,
-                    if_ajax_for_api: bool = True,
-                    if_json_for_api: bool = False,
-                    if_multipart_for_api: bool = False
-                    ) -> dict:
-
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-        url_path = urllib.parse.urlparse(host_url).path
-        host_headers = {
-            'Referer' if if_referer_for_host else 'Host': host_url,
-            "User-Agent": user_agent,
-        }
-        api_headers = {
-            'Origin': host_url.split(url_path)[0] if url_path else host_url,
-            'Referer': host_url,
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            "User-Agent": user_agent,
-        }
-        if if_api and not if_ajax_for_api:
-            api_headers.pop('X-Requested-With')
-            api_headers.update({'Content-Type': 'text/plain'})
-        if if_api and if_json_for_api:
-            api_headers.update({'Content-Type': 'application/json'})
-        if if_api and if_multipart_for_api:
-            api_headers.pop('Content-Type')
-        return host_headers if not if_api else api_headers
- 
-
-class BaiduV2(Tse):
-    def __init__(self):
-        super().__init__()
-        self.host_url = 'https://fanyi.baidu.com'
-        self.api_url = 'https://fanyi.baidu.com/v2transapi'
-        self.langdetect_url = 'https://fanyi.baidu.com/langdetect'
-        self.get_sign_old_url = 'https://fanyi-cdn.cdn.bcebos.com/static/translation/pkg/index_bd36cef.js'
-        self.get_sign_url = None
-        self.get_sign_pattern = 'https://fanyi-cdn.cdn.bcebos.com/static/translation/pkg/index_(.*?).js'
-        self.host_headers = self.get_headers(self.host_url, if_api=False)
-        self.api_headers = self.get_headers(self.host_url, if_api=True)
-        self.language_map = None
-        self.session = None
-        self.professional_field = ('common', 'medicine', 'electronics', 'mechanics', 'novel')
-        self.token = None
-        self.sign = None
-        # self.acs_token = None
-        self.query_count = 0
-        self.output_zh = 'zh'
-        self.input_limit = 5000
- 
-    def get_sign(self, query_text, host_html, ss, timeout, proxies):
-        gtk_list = re.compile("""window.gtk = '(.*?)';|window.gtk = "(.*?)";""").findall(host_html)[0]
-        gtk = gtk_list[0] or gtk_list[1]
-
-        try:
-            if not self.get_sign_url:
-                self.get_sign_url = re.compile(self.get_sign_pattern).search(host_html).group()
-            r = ss.get(self.get_sign_url, headers=self.host_headers, timeout=timeout, proxies=proxies)
-            r.raise_for_status()
-        except:
-            r = ss.get(self.get_sign_old_url, headers=self.host_headers, timeout=timeout, proxies=proxies)
-            r.raise_for_status()
-        sign_html = r.text
-
-        begin_label = 'define("translation:widget/translate/input/pGrab",function(r,o,t){'
-        end_label = 'var i=null;t.exports=e});'
-        from js2py import EvalJs
-        sign_js = sign_html[sign_html.find(begin_label) + len(begin_label):sign_html.find(end_label)]
-        sign_js = sign_js.replace('function e(r)', 'function e(r,i)')
-        
-        self.ctx=  EvalJs()
-        self.ctx.execute(sign_js)
-        return self.ctx.e( query_text, gtk)
-
-    def get_tk(self, host_html):
-        tk_list = re.compile("""token: '(.*?)',|token: "(.*?)",""").findall(host_html)[0]
-        return tk_list[0] or tk_list[1]
-
-    # def get_acs_token(self):
-    #     pass
- 
-    def baidu_api(self, query_text: str, from_language: str = 'auto', to_language: str = 'en', **kwargs) :
-        """
-        https://fanyi.baidu.com
-        :param query_text: str, must.
-        :param from_language: str, default 'auto'.
-        :param to_language: str, default 'en'.
-        :param **kwargs:
-                :param timeout: float, default None.
-                :param proxies: dict, default None.
-                :param sleep_seconds: float, default `random.random()`.
-                :param is_detail_result: boolean, default False.
-                :param if_ignore_limit_of_length: boolean, default False.
-                :param limit_of_length: int, default 5000.
-                :param if_ignore_empty_query: boolean, default False.
-                :param update_session_after_seconds: float, default 1500.
-                :param if_show_time_stat: boolean, default False.
-                :param show_time_stat_precision: int, default 4.
-                :param professional_field: str, default 'common'. Choose from ('common', 'medicine', 'electronics', 'mechanics', 'novel')
-        :return: str or dict
-        """
-
-        timeout = kwargs.get('timeout', None)
-        proxies = kwargs.get('proxies', None)
-        is_detail_result = kwargs.get('is_detail_result', False)
-        sleep_seconds = kwargs.get('sleep_seconds', random.random())
-        update_session_after_seconds = kwargs.get('update_session_after_seconds', self.default_session_seconds)
-
-        use_domain = kwargs.get('professional_field', 'common')
-         
-
-        not_update_cond_time = 1 if time.time() - self.begin_time < update_session_after_seconds else 0
-        if not (self.session and not_update_cond_time and self.language_map and self.token and self.sign):
-            self.session = requests.Session()
-            _ = self.session.get(self.host_url, headers=self.host_headers, timeout=timeout, proxies=proxies)  # must twice, send cookies.
-            host_html = self.session.get(self.host_url, headers=self.host_headers, timeout=timeout, proxies=proxies).text
-            self.token = self.get_tk(host_html)
-            self.sign = self.get_sign(query_text, host_html, self.session, timeout, proxies) 
- 
-        if from_language == 'auto':
-            res = self.session.post(self.langdetect_url, headers=self.api_headers, data={"query": query_text}, timeout=timeout, proxies=proxies)
-            from_language = res.json()['lan']
-
-        params = {"from": from_language, "to": to_language}
-        form_data = {
-            "from": from_language,
-            "to": to_language,
-            "query": query_text,  # from urllib.parse import quote_plus
-            "transtype": "realtime",  # ["translang","realtime"]
-            "simple_means_flag": "3",
-            "sign": self.sign,
-            "token": self.token,
-            "domain": use_domain,
-        }
-        form_data = urllib.parse.urlencode(form_data).encode('utf-8')
-        # self.api_headers.update({'Acs-Token': self.acs_token})
-        r = self.session.post(self.api_url, params=params, data=form_data, headers=self.api_headers, timeout=timeout, proxies=proxies)
-       
-        data = r.json()
-        time.sleep(sleep_seconds)
-        self.query_count += 1 
-        return data if is_detail_result else '\n'.join([x['dst'] for x in data['trans_result']['data']])
-
-from traceback import print_exc
-
-from translator.basetranslator import basetrans
 class TS(basetrans):
-    def inittranslator(self):  
-        self.engine=BaiduV2()
-        self.engine._=None
-    def translate(self,content):  
-            return self.engine.baidu_api(content,self.srclang,self.tgtlang)
+     
+    def inittranslator(self)  :  
+        self.headers = {
+            'Accept': '*/*',
+            'Host': 'fanyi.baidu.com',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+             'Origin': 'https://fanyi.baidu.com',
+            'Referer': 'https://fanyi.baidu.com/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+        }
+
+        self.session = requests.Session()
+        #self.session1= httpx.Client(headers=self.headers)
+        index_url = 'https://fanyi.baidu.com/'
         
+        self.session.get(url=index_url, headers=self.headers, proxies=  {'http': None,'https': None},timeout = globalconfig['translatortimeout'] )  #session自动生成cookie
+        response_index = self.session.get(url=index_url ,headers=self.headers, proxies=  {'http': None,'https': None}  )
+        self.starttime=int(time.time()*1000)
+        self.token =   re.findall(r"token: '([0-9a-z]+)'", response_index.text)[0]
+        self.gtk =   re.findall(r'gtk = "(.*?)"', response_index.text)[0]
+        self.systime=int(re.findall(r"systime: '([0-9]+)'", response_index.text)[0])
+        with open(os.path.join('./files/scripts/baidufanyi_encrypt.js'), 'r', encoding='utf-8') as f:
+            baidu_js = f.read()
+         
+        self.ctx=  EvalJs()
+        self.ctx.execute(baidu_js)
+         
+        url = 'https://dlswbr.baidu.com/heicha/mm/2060/acs-2060.js'
+     
+        res = self.session.get(url,headers= 
+     {
+    'authority': 'dlswbr.baidu.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46',
+} ) 
+        array='['+ re.findall('p.run\(\[(.*?)\]\)', res.text, re.DOTALL)[0]+']'
+        array=eval(array) 
+        print(array)
+        self.timestamp =str(array[2][6][1])
+        self.version=array[3][6]
+        self.k1=array[3][7]
+        self.step=20
+        self.k2=array[3][8]
+        print(self.timestamp,self.k1,self.k2,self.version)
+        print(self.session.cookies)
+
+        hm=re.findall(r'hm.src = "//(.*)"', response_index.text)[0]
+                
+        headers = {
+            'Accept': '*/*',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive', 
+            'Pragma': 'no-cache',
+            'Referer': 'https://fanyi.baidu.com/',
+            'Sec-Fetch-Dest': 'script',
+            'Sec-Fetch-Mode': 'no-cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+        }
+
+        _ = self.session.get('https://'+hm, headers=headers)
+        print(self.session.cookies)
+        self.session.cookies.update({
+            'APPGUIDE_10_0_2': '1',
+    'REALTIME_TRANS_SWITCH': '1',
+    'FANYI_WORD_SWITCH': '1',
+    'HISTORY_SWITCH': '1',
+    'SOUND_SPD_SWITCH': '1',
+    'SOUND_PREFER_SWITCH': '1',
+        })
+    def translate(self,query):  
+        
+        #sign =self.jsrun.call('e', query, self.gtk)
+            sign=self.ctx.e(query,self.gtk)
+            translate_url = 'https://fanyi.baidu.com/#'+self.srclang +'/'+self.tgtlang +'/%s' %  ( parse.quote(query))
+             
+            acs_token=self.timestamp+ self.ctx.ascToken(translate_url, self.k1,self.k2,self.version,  str(int((time.time())*1000)-self.starttime+self.systime))
+            #print(acs_token)
+            params = {
+                'from': self.srclang ,
+                'to': self.tgtlang ,
+            }
+            data = {
+                'from': self.srclang ,
+                'to': self.tgtlang ,
+                'query': query,
+                'transtype': 'realtime',
+                'simple_means_flag': '3',
+                'sign': sign,
+                'token': self.token,
+                'domain': 'common'
+            }
+            print(data)
+            
+            headers = {
+                'Accept': '*/*',
+                'Host': 'fanyi.baidu.com',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Origin': 'https://fanyi.baidu.com',
+                'Referer': 'https://fanyi.baidu.com/',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+            }
+             
+            _ = self.session.post(url='https://fanyi.baidu.com/langdetect',headers=headers,   data={'query':query},timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None})
+
+
+            headers["Acs-Token"]=acs_token
+            response = self.session.post(url='https://fanyi.baidu.com/v2transapi',headers=headers,params=params,   data=data,timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None})
+                        
+            headers.pop('Acs-Token')
+            params = {
+                'req': 'check',
+                'fanyi_src':query,
+                'direction': self.srclang+'2'+ self.tgtlang,
+                '_': self.systime+self.step,
+            }
+            self.step+=1
+            _ = self.session.get('https://fanyi.baidu.com/pcnewcollection', params=params,  headers=headers)
+            print(response.json())
+            result ='\n'.join([_['dst'] for _ in response.json()['trans_result']['data']])  
+            # params = {
+            #     'req': 'check',
+            #     'fanyi_src': query,
+            #     'direction': self.srclang +'2'+self.tgtlang ,
+            #     '_': int(time.time()*1000),
+            # }
+             
+            #response = self.session.get('https://fanyi.baidu.com/pcnewcollection', params=params,timeout = globalconfig['translatortimeout'],proxies=  {'http': None,'https': None} )
+            return result
+         
+        
+    def show(self,res):
+        print('百度','\033[0;32;47m',res,'\033[0m',flush=True)
+     
