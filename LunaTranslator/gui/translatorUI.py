79f865816bff27e7c609ae13a5953b3811b48d2e
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-01-31 15:43:46 +0800

    1

--- LunaTranslator/gui/translatorUI.py
+++ LunaTranslator/gui/translatorUI.py
@@ -845,12 +845,32 @@ class QUnFrameWindow(QWidget):
             with open('./cache/update/update.bat','w',encoding='utf8') as ff:
                 
                 ff.write(r'''
+
+:rekill1
 taskkill /F /IM LunaTranslator_main.exe
+tasklist | find /i "LunaTranslator_main.exe" && go rekill || echo "fuck"
+:rekill2
 taskkill /F /IM LunaTranslator.exe
-taskkill /F /IM LunaTranslator_no_Admin.exe 
-xcopy .\cache\update\LunaTranslator\ .\ /s /e /c /y /h /r
+tasklist | find /i "LunaTranslator.exe" && go rekill || echo "fuck"
+:rekill
+taskkill /F /IM LunaTranslator_no_Admin.exe  
+tasklist | find /i "LunaTranslator_no_Admin.exe" && go rekill || echo "fuck"
+
+:trydel1
+del LunaTranslator\LunaTranslator_main.exe
+if exist LunaTranslator\LunaTranslator_main.exe goto trydel1
+:trydel2
+del LunaTranslator_no_Admin.exe
+if exist LunaTranslator_no_Admin.exe goto trydel2
+:trydel3
+del LunaTranslator.exe
+if exist LunaTranslator.exe goto trydel2
+
+xcopy .\cache\update\LunaTranslator\ .\ /s /e /c /y /h /r 
 exit
+                
                 ''') 
-            subprocess.Popen('cache\\update\\update.bat' ,shell=True)
+            #subprocess.Popen('cache\\update\\update.bat' ,shell=True)
+            win32api.ShellExecute(None, "open", 'cache\\update\\update.bat', "", os.path.dirname('.'), win32con.SW_HIDE)
         endsubprocs()
         os._exit(0) 
