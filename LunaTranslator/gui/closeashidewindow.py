c067de3343da715160c0f08810c08eff6df00da3
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-01 23:25:42 +0800

    1

--- LunaTranslator/gui/closeashidewindow.py
+++ LunaTranslator/gui/closeashidewindow.py
@@ -7,12 +7,21 @@ class closeashidewindow(QMainWindow):
     def __init__(self, args) -> None:
         super().__init__(args )
         self.showsignal.connect(self.showfunction) 
+        self.savelastgeo=None
     def showfunction(self): 
         if self.isMinimized():
-            self.showNormal()
+            self.showNormal() 
         elif self.isHidden(): 
-            self.show() 
+            self.show()  
         else:
-            self.hide() 
+            self.hide()  
+    def showEvent(self, a0 ) -> None:
+        if self.savelastgeo:
+            self.setGeometry(self.savelastgeo)
+        super().showEvent(a0)
+    def hideEvent(self, a0 ) -> None:
+        self.savelastgeo=self.geometry()
+        super().hideEvent(a0)
     def closeEvent(self, event) :  
-        self.hide()
+        self.hide() 
+        self.savelastgeo=self.geometry()
