8cca26ef04d92c4c5551a5b6e6aace9729f67675
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-08 11:07:26 +0800

    1

--- LunaTranslator/embedded/inject.py
+++ LunaTranslator/embedded/inject.py
@@ -3,6 +3,7 @@
 # 2/3/2013 jichi
 # Windows only
 import os
+from utils.subproc import subproc
 def inject_vnragent(pid): 
   ret = True
   for dllpath in ['msvcr100.dll',
@@ -10,9 +11,9 @@ def inject_vnragent(pid):
                   'qtcore4.dll',
                   'qtnetwork4.dll',
                   'vnragent']:
-    dllpath+'./files/embedded/'
+    dllpath='./files/embedded/'+dllpath
     dllpath = os.path.abspath(dllpath)
     
     print('.\\files\\embedded\\dllinject32.exe '+str(pid)+' "'+dllpath+'"')
-    os.system('.\\files\\embedded\\dllinject32.exe '+str(pid)+' "'+dllpath+'"')  
+    subproc('.\\files\\embedded\\dllinject32.exe '+str(pid)+' "'+dllpath+'"')  
   return ret
--- LunaTranslator/embedded/main.py
+++ LunaTranslator/embedded/main.py
@@ -1,16 +1,14 @@
 import time
-import sys
-if sys.version_info.major==3:
-    from PyQt5.QtCore import QCoreApplication 
-    from PyQt5.QtWidgets import QMainWindow,QApplication
-    from rpcman3 import RpcServer
-    from gameagent3 import GameAgent 
+import sys  
+from PyQt5.QtWidgets import QMainWindow,QApplication
+from rpcman3 import RpcServer
+from gameagent3 import GameAgent 
 if __name__=="__main__":
     app =  QApplication(sys.argv) 
     rpc=RpcServer()
     rpc.start()   
     ga=GameAgent(rpc)
-    ga.attachProcess(pid=4488)
+    ga.attachProcess(pid=16256)
     rpc.engineTextReceived.connect(ga.sendEmbeddedTranslation)
     rpc.clearAgentTranslation()
     x=QMainWindow()
