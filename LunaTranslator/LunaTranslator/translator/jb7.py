1e52037676f6befd09761b60d8c883b1bb0bc239
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-11 18:59:33 +0800

    1

--- LunaTranslator/LunaTranslator/translator/jb7.py
+++ LunaTranslator/LunaTranslator/translator/jb7.py
@@ -54,7 +54,7 @@ class TS(basetrans):
             pipename='\\\\.\\Pipe\\jbj7_'+t
             waitsignal='jbjwaitload_'+t
             #self.engine=subproc(f'./files/x64_x86_dll/jbj7.exe "{self.dllpath}"'+dictpath,stdin=subprocess.PIPE,name='jbj', stdout=subprocess.PIPE ,encoding='utf-16-le')
-            self.engine=subproc(f'C:/Users/11737/Documents/GitHub/LunaTranslator/CXXplugins/win32dllforward/Release/jbj7_4.exe "{self.dllpath}" {pipename} {waitsignal} '+dictpath,name='jbj7',  stdout=subprocess.PIPE , stdin=subprocess.PIPE ,encoding='utf-16-le') 
+            self.engine=subproc(f'./files/x64_x86_dll/jbj7_4.exe "{self.dllpath}" {pipename} {waitsignal} '+dictpath,name='jbj7',  stdout=subprocess.PIPE , stdin=subprocess.PIPE ,encoding='utf-16-le') 
             attr=win32security.SECURITY_DESCRIPTOR(win32con.SECURITY_DESCRIPTOR_REVISION)
             attr.SetSecurityDescriptorDacl(True,None,False) 
             secu=win32security.SECURITY_ATTRIBUTES() 
