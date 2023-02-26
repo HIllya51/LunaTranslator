017e493328c65f59045ba9944b5f814f52215939
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-02-26 12:59:13 +0800

    1

--- LunaTranslator/LunaTranslator/textsource/textsourcebase.py
+++ LunaTranslator/LunaTranslator/textsource/textsourcebase.py
@@ -95,10 +95,8 @@ class basetext:
             try:
                 t=self.gettextthread()
                 if t and globalconfig['autorun']:
-                    if type(t)==str:
-                        self.textgetmethod(t) 
-                    else:
-                        self.textgetmethod(*t)
+                    self.textgetmethod(t) 
+                    
             except: 
                 print_exc() 
             
