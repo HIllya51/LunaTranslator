ffc39997d19739217b831d58a0d3c6f667108822
Liaocfe <liaocfe@gmail.com>
2023-03-03 14:26:01 +0800

    fix openai client

--- LunaTranslator/LunaTranslator/translator/openai.py
+++ LunaTranslator/LunaTranslator/translator/openai.py
@@ -5,27 +5,12 @@ import openai
 from translator.basetranslator import basetrans
 
 
-class TS(basetrans):
-    def langmap(self):
-        return {"cht": "zh-Hant"}
+class OpenAIClient:
+    def __init__(self, api_key, temperature):
+        self.api_key = api_key
+        self.temperature = temperature
 
-    def translate(self, query):
-        if self.config['SECRET_KEY'].strip() == "":
-            return
-        else:
-            secret_key = self.config['SECRET_KEY'].strip()
-
-        temperature = 0.3
-        if self.config['temperature'].strip() != "":
-            try:
-                temperature = float(self.config['temperature'].strip())
-            except:
-                temperature = 0.3
-
-        openai.api_key = secret_key
-
-        content = "translate {fr} to {to}: ".format(fr=self.srclang, to=self.tgtlang) + query
-
+    def ChatCompletion(self, content):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -36,9 +21,41 @@ class TS(basetrans):
             n=1,
             stop=None,
             top_p=1,
-            temperature=0,
+            temperature=self.temperature,
             stream=False
         )
+        return response
+
+
+client = OpenAIClient("", 0.3)
+
+
+class TS(basetrans):
+    def langmap(self):
+        return {"zh": "zh-CN", "cht": "zh-TW"}
+
+    def translate(self, query):
+        if self.config['SECRET_KEY'].strip() == "":
+            return
+        else:
+            secret_key = self.config['SECRET_KEY'].strip()
+            if secret_key != client.api_key:
+                client.api_key = secret_key
+                # 对api_key频繁赋值会消耗性能
+                openai.api_key = client.api_key
+
+        if self.config['temperature'].strip() != "":
+            try:
+                temperature = float(self.config['temperature'].strip())
+                if temperature != client.temperature:
+                    client.temperature = temperature
+            except:
+                client.temperature = 0.3
+
+        content = "translate {fr} to {to}: ".format(fr=self.srclang, to=self.tgtlang) + query
+
+        response = client.ChatCompletion(content)
+
         message = response['choices'][0]['message']['content'].replace('\n\n', '\n').strip()
 
         return message
