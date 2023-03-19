d9956886445890132d569990a4dda95c4d475b62
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-03-19 17:19:16 +0800

    1

--- LunaTranslator/LunaTranslator/translator/openai.py
+++ LunaTranslator/LunaTranslator/translator/openai.py
@@ -10,9 +10,10 @@ class TS(basetrans):
         return {"zh": "zh-CN", "cht": "zh-TW"}
     def inittranslator(self):
         self.api_key=None
+        self.context=[]
     def translate(self, query):
         self.checkempty(['SECRET_KEY','model'])
-        
+        self.contextnum=int(self.config['附带上下文个数'])
         secret_key = self.config['SECRET_KEY'] 
         if secret_key != self.api_key:
             self.api_key = secret_key
@@ -24,14 +25,21 @@ class TS(basetrans):
             temperature = float(self.config['Temperature'])
         except:
             temperature = 0.3
+        
+        message=[
+            {"role": "system", "content": "You are a translator"},
+                {"role": "user", "content": f"translate from {self.srclang} to {self.tgtlang}"},
+        ]
+        
+        for _i in range(min(len(self.context)//2 ,self.contextnum)):
+            i=len(self.context)//2-min(len(self.context)//2 ,self.contextnum)+_i
+            message.append(self.context[i*2])
+            message.append(self.context[i*2+1])
+        message.append({"role": "user", "content": query}) 
 
         response = openai.ChatCompletion.create(
             model=self.config['model'],
-            messages=[
-                {"role": "system", "content": "You are a translator"},
-                {"role": "user", "content": f"translate from {self.srclang} to {self.tgtlang}"},
-                {"role": "user", "content": query}
-            ],
+            messages=message,
             # optional
             max_tokens=2048,
             n=1,
@@ -40,11 +48,15 @@ class TS(basetrans):
             temperature=temperature,
             stream=False
         )
-           
+        
 
         try:
             message = response['choices'][0]['message']['content'].replace('\n\n', '\n').strip()
-
+            self.context.append( {"role": "user", "content": query})
+            self.context.append({
+                'role':"assistant",
+                "content":message
+            })
             return message
-        except:
+        except: 
             raise Exception(json.dumps(response))
