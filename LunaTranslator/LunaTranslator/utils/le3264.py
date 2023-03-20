c1c3a295c7b7732f9c60dc791e3799a8d982bab9
HIllya51 <101191390+HIllya51@users.noreply.github.com>
2023-03-21 00:09:53 +0800

    1

--- LunaTranslator/LunaTranslator/utils/le3264.py
+++ LunaTranslator/LunaTranslator/utils/le3264.py
@@ -4,12 +4,9 @@ def le3264run(game,alwaysuselr):
         b=win32utils.GetBinaryType(game)
         if b==0 and alwaysuselr==False: 
             #le=os.path.join(os.path.abspath(globalconfig['LocaleEmulator']),'LEProc.exe')
-            le=os.path.join(os.path.abspath('./files/plugins/Locale.Emulator.2.5.0.1'),'LEProc.exe')
+            le=os.path.join(os.path.abspath('./files/plugins/Locale.Emulator'),'cleproc.exe')
             if os.path.exists(le): 
-                    if os.path.exists(game+'.le.config')==False:
-                            writeleconfig(game)
-                                
-                    win32utils.ShellExecute(None, "open", le, f'-run "{game}"', os.path.dirname(game), win32con.SW_SHOW)
+                    win32utils.ShellExecute(None, "open", le, f'"{game}"', os.path.abspath('./files/plugins/Locale.Emulator'), win32con.SW_SHOW)
         elif b==6 or alwaysuselr==True: 
                 #le=os.path.join(os.path.abspath(globalconfig['Locale_Remulator']),'LRProc.exe')
                 le=os.path.join(os.path.abspath('./files/plugins/Locale_Remulator.1.5.1'),'LRProc.exe')
@@ -20,23 +17,3 @@ def le3264run(game,alwaysuselr):
     except:
         win32utils.ShellExecute(None, "open", game, '', os.path.dirname(game), win32con.SW_SHOW) 
     
-
-def writeleconfig(game):
-         
-        leconfig='''<?xml version="1.0" encoding="utf-8"?>
-<LEConfig>
-  <Profiles>
-    <Profile Name="%s.le.config" Guid="%s" MainMenu="false">
-      <Parameter></Parameter>
-      <Location>ja-JP</Location>
-      <Timezone>Tokyo Standard Time</Timezone>
-      <RunAsAdmin>false</RunAsAdmin>
-      <RedirectRegistry>true</RedirectRegistry>
-      <IsAdvancedRedirection>false</IsAdvancedRedirection>
-      <RunWithSuspend>false</RunWithSuspend>
-    </Profile>
-  </Profiles>
-</LEConfig>
-'''     
-        with open(game+'.le.config','w',encoding='utf8') as ff :
-                ff.write(leconfig %(os.path.basename(game),uuid.uuid1()))
