6e6c1831373b6cd26f4198c6385187b79a3d0d7f
chaihahaha <chai836275709@gmail.com>
2023-10-10 22:34:14 +0800

    Transformers Neural Translator: Create transformers_neural.py

---
+++ LunaTranslator/LunaTranslator/translator/transformers_neural.py
@@ -0,0 +1,20 @@
+from translator.basetranslator import basetrans
+from transformers import pipeline
+
+
+class TS(basetrans):
+    def langmap(self):
+        return {"zh": "zh-CN", "cht": "zh-TW"}
+
+    def translate_sentence(self, sentence):
+        return self.pipeline(f'<-ja2zh-> {sentence}')[0]['translation_text']
+
+    def inittranslator(self):
+        assert not self.checkempty(['模型路径','推理设备'])
+        model_path = self.config['模型路径']
+        device = self.config['推理设备']
+        self.pipeline = pipeline("translation", model=model_path,device=device)
+
+    def translate(self, content):
+        translated = self.translate_sentence(content)
+        return translated
