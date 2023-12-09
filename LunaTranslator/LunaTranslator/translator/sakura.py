a6d9880ee13bf1a12b055bbee000d8c1a624aa0c
sakura <sakura@srpr.moe>
2023-12-09 18:41:19 +0800

    feat: add v0.9 model support

--- LunaTranslator/LunaTranslator/translator/sakura.py
+++ LunaTranslator/LunaTranslator/translator/sakura.py
@@ -33,11 +33,11 @@ class TS(basetrans):
             
         return prompt
         
-    def make_request(self, prompt):
+    def make_request(self, prompt, is_test=False):
         if self.api_type == "llama.cpp":
             request = {
                 "prompt": prompt,
-                "n_predict": int(self.config['max_new_token']),
+                "n_predict": 1 if is_test else int(self.config['max_new_token']),
                 "temperature": float(self.config['temperature']),
                 "top_p": float(self.config['top_p']),
                 "repeat_penalty": float(self.config['repetition_penalty']),
@@ -46,10 +46,10 @@ class TS(basetrans):
                 "seed": -1
             }
             return request
-        elif self.api_type == "dev_server":
+        elif self.api_type == "dev_server" or is_test:
             request = {
                 "prompt": prompt,
-                "max_new_tokens": int(self.config['max_new_token']),
+                "max_new_tokens": 1 if is_test else int(self.config['max_new_token']),
                 "do_sample": bool(self.config['do_sample']),
                 "temperature": float(self.config['temperature']),
                 "top_p": float(self.config['top_p']),
@@ -61,9 +61,9 @@ class TS(basetrans):
             }
             return request
         elif self.api_type == "openai_like":
-            raise NotImplementedError
+            raise NotImplementedError(f"1: {self.api_type}")
         else:
-            raise NotImplementedError
+            raise NotImplementedError(f"2: {self.api_type}")
     def parse_output(self, output: str, length):
         output = output.strip()
         output_list = output.split("\n")
@@ -82,33 +82,35 @@ class TS(basetrans):
                 output =  response['content']
                 new_token = response['tokens_predicted']
             else:
-                raise NotImplementedError
+                raise NotImplementedError("3")
         except Exception as e:
-            raise Exception(str(e) + f"\n{self.api_type}, {self.api_url}, {self.model_type}\n与API接口通信失败，请检查设置的API服务器监听地址是否正确，或检查API服务器是否正常开启。")
-        return output, new_token
+            raise Exception(str(e) + f"\napi_type: '{self.api_type}', api_url: '{self.api_url}', model_type: '{self.model_type}'\n与API接口通信失败，请检查设置的API服务器监听地址是否正确，或检查API服务器是否正常开启。")
+        return output, new_token, response
     def set_model_type(self):
         #TODO: get model type from api
         self.model_type = "NotImplemented"
+        request = self.make_request("test", is_test=True)
+        _, _, response = self.do_post(request)
+        if self.api_type == "llama.cpp":
+            model_name: str = response['model']
+            model_version = model_name.split("-")[-2]
+            if "0.8" in model_version:
+                self.model_type = "baichuan"
+            elif "0.9" in model_version:
+                self.model_type = "qwen"
         return
     def set_api_type(self):
         endpoint = self.config['API接口地址']
         if endpoint[-1] != "/":
             endpoint += "/"
         api_url = endpoint + "api/v1/generate"
-        test_json = {
-            "prompt": "将下面的日文文本翻译成中文：これはただのテストで、ギャルゲーの通訳ではありません。",
-            "max_new_tokens": 4,
-            "do_sample": bool(self.config['do_sample']),
-            "temperature": float(self.config['temperature']),
-            "top_p": float(self.config['top_p']),
-            "repetition_penalty": float(self.config['repetition_penalty']),
-            "num_beams": int(self.config['num_beams']),
-            "frequency_penalty": float(self.config['frequency_penalty']),
-            "top_k": 40,
-            "seed": -1
-        }
+        test_json = self.make_request("test", is_test=True)
         try:
-            response = self.session.post(api_url, json=test_json).json()
+            response = self.session.post(api_url, json=test_json)
+        except Exception as e:
+            raise Exception(str(e) + f"\napi_type: '{self.api_type}', api_url: '{self.api_url}', model_type: '{self.model_type}'\n与API接口通信失败，请检查设置的API服务器监听地址是否正确，或检查API服务器是否正常开启。")
+        try:
+            response = response.json()
             output = response['results'][0]['text']
             new_token = response['results'][0]['new_token']
             self.api_type = "dev_server"
@@ -128,14 +130,14 @@ class TS(basetrans):
         request = self.make_request(prompt)
 
         if not self.config['利用上文信息翻译（通常会有一定的效果提升，但会导致变慢）']:
-            output, new_token = self.do_post(request)
+            output, new_token, _ = self.do_post(request)
             
             if bool(self.config['fix_degeneration']):
                 cnt = 0
                 while new_token == self.config['max_new_token']:
                     # detect degeneration, fixing
                     request['frequency_penalty'] += 0.1
-                    output, new_token = self.do_post(request)
+                    output, new_token, _ = self.do_post(request)
                     
                     cnt += 1
                     if cnt == 2:
@@ -143,14 +145,14 @@ class TS(basetrans):
         else:
             query_list = self.sliding_window(query)
             request['prompt'] = self.make_prompt(query)
-            output, new_token = self.do_post(request)
+            output, new_token, _ = self.do_post(request)
             
             if bool(self.config['fix_degeneration']):
                 cnt = 0
                 while new_token == self.config['max_new_token']:
                     # detect degeneration, fixing
                     request['frequency_penalty'] += 0.1
-                    output, new_token = self.do_post(request)
+                    output, new_token, _ = self.do_post(request)
                     
                     cnt += 1
                     if cnt == 2:
@@ -159,5 +161,5 @@ class TS(basetrans):
             output = self.parse_output(output, len(query_list))
             if not output:
                 request['prompt'] = self.make_prompt(query)
-                output, new_token = self.do_post(request)
+                output, new_token, _ = self.do_post(request)
         return output
