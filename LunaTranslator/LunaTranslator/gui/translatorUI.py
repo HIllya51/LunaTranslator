d4c1db815d26616a322c1bb5b9d59a68de3bac7b
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-03-07 16:35:53 +0800

    Update translatorUI.py

--- LunaTranslator/LunaTranslator/gui/translatorUI.py
+++ LunaTranslator/LunaTranslator/gui/translatorUI.py
@@ -170,6 +170,7 @@ class QUnFrameWindow(resizableframeless):
                 self.translate_text.insertatpointer(self.saveiterclasspointer[iter_context_class]['start'],text)
                 self.saveiterclasspointer[iter_context_class]['curr']=self.translate_text.getcurrpointer()
                 self.document.blockSignals(False)
+                self.textAreaChanged()
             if globalconfig['zitiyangshi'] ==3:
                 self.translate_text.showyinyingtext2(color,iter_context_class,self.saveiterclasspointer[iter_context_class]['start'],text) 
             return
