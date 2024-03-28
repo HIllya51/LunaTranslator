eeb4058dbe93a6b0831ac7c7ed72396e0cf72324
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-03-29 02:42:25 +0800

    issues/609

--- LunaTranslator/LunaTranslator/translator/google_dev.py
+++ LunaTranslator/LunaTranslator/translator/google_dev.py
@@ -13,5 +13,5 @@ class TS(basetransdev):
             self.Page_navigate('https://translate.google.com/?sl={}&tl={}'.format(self.srclang,self.tgtlang))
             self.lastlang=(self.srclang,self.tgtlang)
         self.Runtime_evaluate('textarea=document.querySelector("textarea");textarea.value="";event = new Event("input", {{bubbles: true, cancelable: true }});textarea.dispatchEvent(event);textarea=document.querySelector("textarea");textarea.value=`{}`;event = new Event("input", {{bubbles: true, cancelable: true }});textarea.dispatchEvent(event);'.format(content))
-        return self.wait_for_result('document.querySelector("#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb.EjH7wc > div.AxqVh > div.OPPzxe > c-wiz.sciAJc > div > div.usGWQd > div > div.lRu31").innerText') 
+        return self.wait_for_result('document.querySelector("#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > c-wiz.sciAJc > div > div.usGWQd > div > div.lRu31").innerText') 
         
