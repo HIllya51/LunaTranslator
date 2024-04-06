5dcec35687a5b258a9ee267b80c0a70b5f37702a
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-06 22:50:04 +0800

    fix

--- plugins/exec/luna.cpp
+++ plugins/exec/luna.cpp
@@ -5,13 +5,18 @@
 
 int main()
 {
-	if (!std::filesystem::exists(L".\\LunaTranslator\\LunaTranslator_main.exe"))
+	TCHAR szPath[MAX_PATH];
+	GetModuleFileName(NULL, szPath, ARRAYSIZE(szPath));
+	std::wstring moduleName = szPath;
+	auto currpath = moduleName.substr(0, moduleName.rfind(L'\\'));
+	auto exe = currpath + L".\\LunaTranslator\\LunaTranslator_main.exe";
+	if (!std::filesystem::exists(exe))
 	{
-		MessageBoxW(0, L"Cant' find ./LunaTranslator/LunaTranslator_main.exe, please download again!", L"Error", 0);
+		MessageBoxW(0, exe.c_str(), L"Error", 0);
 		return 0;
 	}
 	STARTUPINFO _1 = {};
 	PROCESS_INFORMATION _2;
-	CreateProcessW(L".\\LunaTranslator\\LunaTranslator_main.exe", NULL, NULL, NULL, FALSE, 0, NULL, L".\\", &_1, &_2);
+	CreateProcessW(exe.c_str(), NULL, NULL, NULL, FALSE, 0, NULL, currpath.c_str(), &_1, &_2);
 	return 0;
 }
