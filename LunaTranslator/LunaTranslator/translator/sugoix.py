ba5abb5c041cab91ecb54e68cc157b3cc1e5d0c0
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-16 14:18:58 +0800

    ...

---
+++ LunaTranslator/LunaTranslator/translator/sugoix.py
@@ -0,0 +1,18 @@
+import requests, os
+from translator.basetranslator import basetrans
+from myutils.subproc import subproc_w, autoproc
+
+
+class TS(basetrans):
+    def translate(self, query):
+
+        json_data = {
+            "content": query,
+            "message": "translate sentences",
+        }
+
+        response = self.session.post(self.config["api"], json=json_data)
+        try:
+            return response.json()
+        except:
+            raise Exception(response.text)
