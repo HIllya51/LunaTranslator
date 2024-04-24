7144148074c084fc5aab75c07a5bc1b10a78ad29
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-24 17:33:41 +0800

    fix
    Update textbrowser.py
    
    Update textbrowser.py

--- LunaTranslator/LunaTranslator/gui/textbrowser.py
+++ LunaTranslator/LunaTranslator/gui/textbrowser.py
@@ -393,7 +393,13 @@ class Textbrowser:
 
                 index = linei
                 _ = self.yinyinglabels[index]
-                _.move(tl1)
+                if self.align:#不然有时候有一个微小的错位
+                    fm = QFontMetricsF(self.textbrowser.currentCharFormat().font())
+                    w = fm.width(block.text()[s : s + l])
+                    WW = self.textbrowser.width()
+                    _.move(int(WW / 2 - w / 2), tl1.y())
+                else:
+                    _.move(tl1)
                 _.setText(block.text()[s : s + l])
                 _.setFont(self.textbrowser.currentCharFormat().font())
 
