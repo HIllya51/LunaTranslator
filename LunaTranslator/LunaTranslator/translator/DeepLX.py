d256d19bf51c04bbdba3949428e55770ab0c20a2
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-27 15:44:51 +0800

    deeplx

---
+++ LunaTranslator/LunaTranslator/translator/DeepLX.py
@@ -0,0 +1,25 @@
+from myutils.config import static_data
+import time
+from translator.basetranslator import basetrans
+
+
+class TS(basetrans):
+    def langmap(self):
+        x = {_: _.upper() for _ in static_data["language_list_translator_inner"]}
+        x.pop("cht")
+        return x  # {"zh":"ZH","ja":"JA","en":"EN","es":"ES","fr":"FR","ru":"RU"}
+
+    def translate(self, query):
+
+        payload = {
+            "text": query,
+            "source_lang": self.srclang,
+            "target_lang": self.tgtlang,
+        }
+
+        response = self.session.post(self.config["api"], json=payload)
+
+        try:
+            return response.json()["data"]
+        except:
+            raise Exception(response.text)
