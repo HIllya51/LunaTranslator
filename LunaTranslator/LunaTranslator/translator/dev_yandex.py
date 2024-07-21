26500e6ffcab144827642fab8ee493c0639b40b5
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-07-21 16:11:47 +0800

    .

--- LunaTranslator/LunaTranslator/translator/dev_yandex.py
+++ LunaTranslator/LunaTranslator/translator/dev_yandex.py
@@ -4,15 +4,13 @@ from translator.basetranslator_dev import basetransdev
 class TS(basetransdev):
     target_url = "https://translate.yandex.com/"
 
-    def inittranslator(self):
-        self.last = ""
-
     def translate(self, content):
+        self.Runtime_evaluate(
+            'document.querySelector("#translation > span").innerText=""'
+        )
         self.Runtime_evaluate('document.querySelector("#fakeArea").innerText=""')
         self.Runtime_evaluate('document.querySelector("#fakeArea").click()')
         self.send_keys(content)
-        last = self.wait_for_result(
-            'document.querySelector("#translation > span").innerText', self.last
+        return self.wait_for_result(
+            'document.querySelector("#translation > span").innerText'
         )
-        self.last = last
-        return self.last
