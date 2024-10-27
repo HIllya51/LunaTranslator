789058871081ec24340fbe651e939c892cd65ef8
test123456654321 <16307130148@fudan.edu.cn>
2024-10-27 22:47:03 +0800

    ds

--- src/LunaTranslator/translator/dev_deepseek.py
+++ src/LunaTranslator/translator/dev_deepseek.py
@@ -4,9 +4,9 @@ from translator.dev_llm_common import commonllmdev
 class TS(commonllmdev):
     target_url = "https://chat.deepseek.com/"
     jsfile = "commonhookxhrstream.js"
-    function1 = "url.endsWith('v0/chat/completions')"
+    function1 = "url.endsWith('v0/chat/completion')"
     function2 = r"""if(!!(chunk.choices[0].delta.content))
                         thistext += chunk.choices[0].delta.content"""
 
     textarea_selector = "#chat-input"
-    button_selector = "._89d4d19:nth-child(3)"
+    button_selector = 'div[role="button"]'
