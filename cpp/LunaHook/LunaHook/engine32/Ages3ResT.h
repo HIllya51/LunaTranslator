7c885c5a90bfe2536239c1fb7a8c657b07d5721f
恍兮惚兮 <1173718158@qq.com>
2024-11-08 19:19:29 +0800

    INFO

--- cpp/LunaHook/LunaHook/engine32/Ages3ResT.h
+++ cpp/LunaHook/LunaHook/engine32/Ages3ResT.h
@@ -5,7 +5,8 @@ class Ages3ResT : public ENGINE
 public:
     Ages3ResT()
     {
-
+        // Muv-Luv_Alternative16_20th
+        // マブラヴ オルタネイティヴ age20th Edition
         check_by = CHECK_BY::FILE;
         check_by_target = L"Ages3ResT.dll";
     };
