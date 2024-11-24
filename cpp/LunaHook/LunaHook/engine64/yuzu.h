330070f0f24cdf6d48f21a4b5f28539965a662a1
恍兮惚兮 <1173718158@qq.com>
2024-11-24 16:19:40 +0800

    .

--- cpp/LunaHook/LunaHook/engine64/yuzu.h
+++ cpp/LunaHook/LunaHook/engine64/yuzu.h
@@ -10,7 +10,9 @@ public:
         check_by = CHECK_BY::CUSTOM;
         check_by_target = []()
         {
-            return (wcscmp(processName_lower, L"suyu.exe") == 0 || wcscmp(processName_lower, L"yuzu.exe") == 0 || wcscmp(processName_lower, L"sudachi.exe") == 0);
+            auto exes = {L"suyu.exe", L"yuzu.exe", L"sudachi.exe", L"citron.exe"};
+            return std::any_of(exes.begin(), exes.end(), [](const wchar_t *e)
+                               { return wcscmp(processName_lower, e) == 0; });
         };
     };
     bool attach_function();
