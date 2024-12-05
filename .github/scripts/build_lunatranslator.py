0babde274f14d451817f26030202f91fe395084c
恍兮惚兮 <1173718158@qq.com>
2024-12-05 14:33:15 +0800

    .

--- .github/scripts/build_lunatranslator.py
+++ .github/scripts/build_lunatranslator.py
@@ -369,7 +369,6 @@ if __name__ == "__main__":
         shutil.copy("../builds/_x86/LunaOCR32.dll", "../../py/files/plugins/DLL32")
         os.makedirs("../../py/files/plugins/DLL64", exist_ok=True)
         shutil.copy("../builds/_x64/shareddllproxy64.exe", "../../py/files/plugins")
-        shutil.copy("../builds/_x64/hookmagpie.dll", "../../py/files/plugins")
         shutil.copy("../builds/_x64/winrtutils64.dll", "../../py/files/plugins/DLL64")
         shutil.copy(
             "../builds/_x64/winsharedutils64.dll", "../../py/files/plugins/DLL64"
