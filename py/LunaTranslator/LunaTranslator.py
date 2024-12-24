ad454194f676caf222d56988ce7ff2a8207e5f2e
恍兮惚兮 <1173718158@qq.com>
2024-12-25 02:21:46 +0800

    .

--- py/LunaTranslator/LunaTranslator.py
+++ py/LunaTranslator/LunaTranslator.py
@@ -387,8 +387,9 @@ class MAINUI:
         maybehaspremt = {}
         skip_other_on_success = False
         fix_rank = globalconfig["fix_translate_rank_rank"].copy()
+        print(is_auto_run , globalconfig["fanyi"]["rengong"].get("manual", False))
         if ("rengong" in self.translators) and (
-            not (is_auto_run and globalconfig["rengong"]["premt"].get("manual", False))
+            not (is_auto_run and globalconfig["fanyi"]["rengong"].get("manual", False))
         ):
             contentraw = self.analyzecontent(text_solved, optimization_params)
             try:
