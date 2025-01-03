4d9840ae681d77131591c139c4aeecf4a994811f
恍兮惚兮 <1173718158@qq.com>
2025-01-04 07:28:06 +0800

    .

--- cpp/scripts/build64.bat
+++ cpp/scripts/build64.bat
@@ -4,4 +4,4 @@ cmake --build ..\build\x64 --config Release --target ALL_BUILD -j 14
 
 copy ..\builds\_x64\shareddllproxy64.exe ..\..\py\files\plugins
 copy ..\builds\_x64\hookmagpie.dll ..\..\py\files\plugins
-robocopy ..\builds\_x64 ..\..\py\files\plugins\DLL64
+robocopy ..\builds\_x64 ..\..\py\files\plugins\DLL64 *.pyd *.dll
--- py/LunaTranslator/tts/windowstts.py
+++ py/LunaTranslator/tts/windowstts.py
@@ -3,36 +3,19 @@ from tts.basettsclass import TTSbase
 
 
 class TTS(TTSbase):
-    def makemap(self, lsit):
-        _ = {}
-        for i in range(len(lsit)):
-            _[lsit[i]] = i
-        return _
 
     def getvoicelist(self):
         self._7 = winsharedutils.SAPI_List(7)
-        self._7m = self.makemap(self._7)
         self._10 = winsharedutils.SAPI_List(10)
-        self._10m = self.makemap(self._10)
-        if len(self._10):
-            needremove = []
-            for _ in self._7:
-                for _1 in self._10:
-                    _s = _1.split("-")
-                    if len(_s) and _s[0] == _[: len(_s[0])]:
-                        needremove.append(_)
-
-            for _ in needremove:
-                self._7.remove(_)
-        return (self._7 + self._10), (self._7 + self._10)
+        __ = []
+        for _ in range(len(self._7)):
+            __.append((7, _))
+        for _ in range(len(self._10)):
+            __.append((7, _))
+        return __, (self._7 + self._10)
 
     def speak(self, content, rate, voice):
-        if voice in self._10m:
-            version = 10
-            voice_idx = self._10m[voice]
-        else:
-            version = 7
-            voice_idx = self._7m[voice]
+        version, voice_idx = voice
 
         data = winsharedutils.SAPI_Speak(content, version, voice_idx, rate, 100)
         return data
