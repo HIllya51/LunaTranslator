128ae45bf38a006209166ba09ba3168e5386d38e
恍兮惚兮 <1173718158@qq.com>
2025-01-16 20:25:48 +0800

    .

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -17,5 +17,15 @@ export default {
                 window.localStorage.currentlang = path.split('/')[1]
             }
         )
+        router.onAfterRouteChanged = () => {
+            if (!window.location.hostname.startsWith('docs')) return
+            let replacetarget = window.location.protocol + '//image.' + window.location.hostname.substring(5);
+            var images = document.getElementsByTagName('img');
+
+            for (var i = 0; i < images.length; i++) {
+
+                images[i].src = images[i].src.replace('https://image.lunatranslator.org', replacetarget)
+            }
+        };
     }
 }
