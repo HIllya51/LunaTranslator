a79d027ba136bcdee71f63663d7774ca428d1428
HIllya51 <1173718158@qq.com>
2025-01-28 13:54:52 +0800

    .

---
+++ src/cpp/winsharedutils/globalmessagelistener.cpp
@@ -0,0 +1,113 @@
+﻿#include "clipboard.hpp"
+static auto LUNA_UPDATE_PREPARED_OK = RegisterWindowMessage(L"LUNA_UPDATE_PREPARED_OK");
+static auto WM_MAGPIE_SCALINGCHANGED = RegisterWindowMessage(L"MagpieScalingChanged");
+static auto WM_SYS_HOTKEY = RegisterWindowMessage(L"SYS_HOTKEY_REG_UNREG");
+bool IsColorSchemeChangeMessage(LPARAM lParam)
+{
+    return lParam && CompareStringOrdinal(reinterpret_cast<LPCWCH>(lParam), -1, L"ImmersiveColorSet", -1, TRUE) == CSTR_EQUAL;
+}
+typedef void (*callbackT)(int, bool, const wchar_t *);
+static int unique_id = 1;
+typedef void (*hotkeycallback_t)();
+static std::map<int, hotkeycallback_t> keybinds;
+struct hotkeymessageLP
+{
+    UINT fsModifiers;
+    UINT vk;
+    hotkeycallback_t callback;
+};
+static LRESULT CALLBACK WNDPROC_1(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    auto callback = (callbackT)GetWindowLongPtrW(hWnd, GWLP_USERDATA);
+    if (callback)
+    {
+        if (WM_SETTINGCHANGE == message)
+        {
+            if (IsColorSchemeChangeMessage(lParam))
+                callback(0, false, NULL);
+        }
+        else if (message == WM_MAGPIE_SCALINGCHANGED)
+        {
+            callback(1, (bool)wParam, NULL);
+        }
+        else if (message == LUNA_UPDATE_PREPARED_OK)
+        {
+            callback(2, false, NULL);
+        }
+        else if (WM_CLIPBOARDUPDATE == message)
+        {
+            auto data = clipboard_get_internal();
+            if (data)
+                callback(3, iscurrentowndclipboard(), data.value().c_str());
+        }
+        else if (WM_HOTKEY == message)
+        {
+            auto _unique_id = (int)(wParam);
+            auto _ = keybinds.find(_unique_id);
+            if (_ == keybinds.end())
+                return 0;
+            _->second();
+        }
+        else if (WM_SYS_HOTKEY == message)
+        {
+            if ((UINT)wParam == 1)
+            {
+                auto info = (hotkeymessageLP *)(lParam);
+                unique_id += 1;
+                auto succ = RegisterHotKey(hWnd, unique_id, info->fsModifiers, info->vk);
+                if (succ)
+                {
+                    keybinds[unique_id] = info->callback;
+                    return unique_id;
+                }
+                return 0;
+            }
+            else
+            {
+                UnregisterHotKey(hWnd, (int)lParam);
+                auto _ = keybinds.find((int)lParam);
+                if (_ != keybinds.end())
+                    keybinds.erase(_);
+            }
+        }
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+static HWND globalmessagehwnd;
+DECLARE_API void startclipboardlisten()
+{
+    addClipboardFormatListener(globalmessagehwnd);
+}
+DECLARE_API void stopclipboardlisten()
+{
+    removeClipboardFormatListener(globalmessagehwnd);
+}
+DECLARE_API void globalmessagelistener(callbackT callback)
+{
+    const wchar_t CLASS_NAME[] = L"globalmessagelistener";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WNDPROC_1;
+    wc.lpszClassName = CLASS_NAME;
+    GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)wc.lpfnWndProc, &wc.hInstance);
+    RegisterClass(&wc);
+    HWND hWnd = CreateWindowEx(0, CLASS_NAME, NULL, 0, 0, 0, 0, 0, 0, nullptr, wc.hInstance, nullptr); // HWND_MESSAGE会收不到。
+    globalmessagehwnd = hWnd;
+    SetWindowLongPtrW(hWnd, GWLP_USERDATA, (LONG_PTR)callback);
+    ChangeWindowMessageFilterEx(hWnd, LUNA_UPDATE_PREPARED_OK, MSGFLT_ALLOW, nullptr);
+    ChangeWindowMessageFilterEx(hWnd, WM_MAGPIE_SCALINGCHANGED, MSGFLT_ALLOW, nullptr);
+}
+DECLARE_API void dispatchcloseevent()
+{
+    PostMessage(HWND_BROADCAST, LUNA_UPDATE_PREPARED_OK, 0, 0);
+}
+DECLARE_API int registhotkey(UINT fsModifiers, UINT vk, hotkeycallback_t callback)
+{
+    auto info = new hotkeymessageLP{fsModifiers, vk, callback};
+    auto ret = SendMessage(globalmessagehwnd, WM_SYS_HOTKEY, 1, (LPARAM)info);
+    delete info;
+    return ret;
+}
+DECLARE_API void unregisthotkey(int _id)
+{
+    SendMessage(globalmessagehwnd, WM_SYS_HOTKEY, 0, (LPARAM)_id);
+}
