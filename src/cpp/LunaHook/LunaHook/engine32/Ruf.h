4e9cecf4c44d19f82895b51d223f9fb8bba46dca
HIllya51 <1173718158@qq.com>
2025-02-23 11:39:41 +0800

    .

--- src/cpp/LunaHook/LunaHook/engine32/Ruf.h
+++ src/cpp/LunaHook/LunaHook/engine32/Ruf.h
@@ -6,8 +6,12 @@ public:
     Ruf()
     {
         is_engine_certain = false;
-        check_by = CHECK_BY::FILE_ALL;
-        check_by_target = check_by_list{L"*.arc", L"*.wsm", L"*.scb", L"*.bmx"};
+        check_by = CHECK_BY::CUSTOM;
+        check_by_target = []()
+        {
+            auto fs = {L"*.arc", L"*.scb", L"*.bmx"};
+            return std::all_of(fs.begin(), fs.end(), Util::CheckFile) && (Util::CheckFile(L"*.wsm") || GetModuleHandle(L"Kagura.dll"));
+        };
     };
     bool attach_function();
 };
