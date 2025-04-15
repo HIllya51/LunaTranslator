3115ded298fe0c06201a5e21c92b368c385c6a5b
HIllya51 <1173718158@qq.com>
2025-04-15 18:51:04 +0800

    .

--- src/LunaTranslator/tts/gptsovits.py
+++ src/LunaTranslator/tts/gptsovits.py
@@ -8,13 +8,17 @@ class TTS(TTSbase):
         return [""], [""]
 
     def speak(self, content, voice, param: SpeechParam):
+        if param.speed > 0:
+            speed = 1 + param.speed / 5
+        else:
+            speed = 1 + param.speed / 15
         js = dict(text=content)
         if self.config["apiv"] == "v2":
             url = urlpathjoin(self.config["URL"], "tts")
-            js.update(text_lang=self.srclang)
+            js.update(text_lang=self.srclang, speed_factor=speed)
         else:
             url = self.config["URL"]
-            js.update(text_language=self.srclang)
+            js.update(text_language=self.srclang, speed=speed)
         extrabody, extraheader = getcustombodyheaders(self.config.get("customparams"))
 
         js.update(extrabody)
