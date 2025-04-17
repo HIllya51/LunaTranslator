cf953c98497812983e8676082af69a30a7068480
HIllya51 <1173718158@qq.com>
2025-04-17 23:35:36 +0800

    .

--- src/cpp/LunaHook/scripts/build32xp_local.bat
+++ src/cpp/LunaHook/scripts/build32xp_local.bat
@@ -1,2 +1,2 @@
-cmake -DBUILD_PLUGIN=OFF -DBUILD_GUI=ON -DBUILD_CLI=ON -DWINXP=1 ../CMakeLists.txt -G "Visual Studio 17 2022" -A win32 -T host=x86 -B ../build/x86_xp
+cmake -DBUILD_GUI=ON -DBUILD_CLI=ON -DWINXP=1 ../CMakeLists.txt -G "Visual Studio 17 2022" -A win32 -T host=x86 -B ../build/x86_xp
 call dobuildxp.bat
--- src/cpp/libs/libs.cmake
+++ src/cpp/libs/libs.cmake
@@ -3,39 +3,35 @@ add_library(nlohmann INTERFACE)
 target_include_directories(nlohmann INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
 option(WINXP "WINXP" OFF)
-option(IS_LUNAHOOK "IS_LUNAHOOK" OFF)
-if(IS_LUNAHOOK)
-    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/minhook ${CMAKE_BINARY_DIR}/minhook)
+
+# add_library(Detours ${CMAKE_CURRENT_LIST_DIR}/Detours/src/creatwth.cpp ${CMAKE_CURRENT_LIST_DIR}/Detours/src/detours.cpp ${CMAKE_CURRENT_LIST_DIR}/Detours/src/modules.cpp ${CMAKE_CURRENT_LIST_DIR}/Detours/src/disasm.cpp)
+# target_include_directories(Detours PUBLIC ${CMAKE_CURRENT_LIST_DIR}/Detours/src)
+
+
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/rapidfuzz-cpp)
+
+include(${CMAKE_CURRENT_LIST_DIR}/mecab.cmake)
+
+
+add_library(webview2 INTERFACE)
+target_include_directories(webview2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/webview2/Microsoft.Web.WebView2.1.0.2535.41/build/native/include)
+if(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
+    target_link_libraries(webview2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/webview2/Microsoft.Web.WebView2.1.0.2535.41/build/native/x64/WebView2LoaderStatic.lib)
 else()
-    # add_library(Detours ${CMAKE_CURRENT_LIST_DIR}/Detours/src/creatwth.cpp ${CMAKE_CURRENT_LIST_DIR}/Detours/src/detours.cpp ${CMAKE_CURRENT_LIST_DIR}/Detours/src/modules.cpp ${CMAKE_CURRENT_LIST_DIR}/Detours/src/disasm.cpp)
-    # target_include_directories(Detours PUBLIC ${CMAKE_CURRENT_LIST_DIR}/Detours/src)
-
-
-    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/rapidfuzz-cpp)
-
-    include(${CMAKE_CURRENT_LIST_DIR}/mecab.cmake)
-
-
-    add_library(webview2 INTERFACE)
-    target_include_directories(webview2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/webview2/Microsoft.Web.WebView2.1.0.2535.41/build/native/include)
-    if(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
-        target_link_libraries(webview2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/webview2/Microsoft.Web.WebView2.1.0.2535.41/build/native/x64/WebView2LoaderStatic.lib)
-    else()
-        target_link_libraries(webview2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/webview2/Microsoft.Web.WebView2.1.0.2535.41/build/native/x86/WebView2LoaderStatic.lib)
-    endif()
-
-    option(USE_VCLTL "USE_VCLTL" ON)
-    if(USE_VCLTL)
-        if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
-            set(LTLPlatform "Win32")
-        endif()
-        if(WINXP)
-            set(WindowsTargetPlatformMinVersion "5.1.2600.0")
-        endif()
-        include("${CMAKE_CURRENT_LIST_DIR}/VC-LTL helper for cmake.cmake")
-    endif()
-
+    target_link_libraries(webview2 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/webview2/Microsoft.Web.WebView2.1.0.2535.41/build/native/x86/WebView2LoaderStatic.lib)
 endif()
+
+option(USE_VCLTL "USE_VCLTL" ON)
+if(USE_VCLTL)
+    if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
+        set(LTLPlatform "Win32")
+    endif()
+    if(WINXP)
+        set(WindowsTargetPlatformMinVersion "5.1.2600.0")
+    endif()
+    include("${CMAKE_CURRENT_LIST_DIR}/VC-LTL helper for cmake.cmake")
+endif()
+ 
 
 if(WINXP)
     add_definitions(-DWINXP=${WINXP})
