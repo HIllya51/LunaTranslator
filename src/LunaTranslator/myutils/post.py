4bd1b12cb65fdcbe90d11fbd8e010d88c5170d0f
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2025-05-01 17:24:08 +0800

    simplify the implementation of _6_fEX() with built-in methods

--- src/LunaTranslator/myutils/post.py
+++ src/LunaTranslator/myutils/post.py
@@ -199,15 +199,8 @@ def _4_f(line):
 
 def _6_fEX(line: str):
     white = getlangsrc().space
-    while True:
-        curr = line
-        for _ in "\r\n\u2028\u2029":
-            line = line.replace(_ + " ", " ").replace(" " + _, " ")
-        if line == curr:
-            break
-
-    line = re.sub(r"[\r\n\u2028\u2029]+", white, line)
-
+    sections = [sec.strip() for sec in line.splitlines()]
+    line = white.join(sec for sec in sections if sec)
     return line
 
 
