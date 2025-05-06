0b912643c5e169892c2713d7e2c0fe57c4e01dcd
HIllya51 <1173718158@qq.com>
2025-05-06 16:33:46 +0800

    .

--- src/cpp/NativeUtils/libwebview2.cmake
+++ src/cpp/NativeUtils/libwebview2.cmake
@@ -22,10 +22,10 @@ if(WINXP)
   string(REPLACE ">>" ">" WebView2EnvironmentOptions "${WebView2EnvironmentOptions}")
   string(REPLACE "Microsoft::WRL::RuntimeClass<\n          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>," "ComImpl<" WebView2EnvironmentOptions "${WebView2EnvironmentOptions}")
   string(REPLACE "Microsoft::WRL::Implements<\n          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>," "ComImpl<" WebView2EnvironmentOptions "${WebView2EnvironmentOptions}")
-  file(WRITE "${webview2_SOURCE_DIR}/WebView2EnvironmentOptions.h" "${WebView2EnvironmentOptions}")
+  file(WRITE "${webview2_include}/WebView2EnvironmentOptions.h" "${WebView2EnvironmentOptions}")
 
-  file(READ "${webview2_SOURCE_DIR}/WebView2.h" WebView2H)
+  file(READ "${webview2_include}/WebView2.h" WebView2H)
   string(REPLACE [=[#include "EventToken.h"]=] "typedef struct EventRegistrationToken\n    {\n    __int64 value;\n    } 	EventRegistrationToken;" WebView2H "${WebView2H}")
-  file(WRITE "${webview2_SOURCE_DIR}/WebView2.h" "${WebView2H}")
+  file(WRITE "${webview2_include}/WebView2.h" "${WebView2H}")
 
 endif()
