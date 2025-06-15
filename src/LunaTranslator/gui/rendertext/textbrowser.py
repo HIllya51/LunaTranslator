da8f6b68dbd37f28f4659e2b1b0eea4ee4aafb80
HIllya51 <1173718158@qq.com>
2025-06-15 16:46:16 +0800

    .

--- src/LunaTranslator/gui/rendertext/textbrowser.py
+++ src/LunaTranslator/gui/rendertext/textbrowser.py
@@ -921,8 +921,14 @@ class TextBrowser(QWidget, dataget):
                 collecti += 1
 
     @property
+    def screen__(self):
+        if gobject.runtime_for_xp:
+            return QApplication.primaryScreen()
+        return self.screen()
+
+    @property
     def maxvisheight(self):
-        return self.screen().virtualGeometry().height() * 2
+        return self.screen__.virtualGeometry().height() * 2
 
     def _showyinyingtext(self, b1, b2, color: ColorControl, font: QFont):
         linei = self.yinyingposline
