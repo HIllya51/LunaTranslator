cb590f1a7d3ac6b7723230c601686dc1d100fe3c
HIllya51 <1173718158@qq.com>
2025-07-08 10:21:52 +0800

    .

--- src/cpp/winxphelper.cmake
+++ src/cpp/winxphelper.cmake
@@ -1,7 +1,6 @@
 ﻿set(YY_Thunks_VERSION "v1.1.7")
 option(WINXP "WINXP" OFF)
 option(WIN10ABOVE "WIN10ABOVE" OFF)
-option(USE_VC_LTL "USE_VC_LTL" OFF)
 
 include(FetchContent)
 FetchContent_Declare(yy_thunks 
@@ -9,12 +8,16 @@ FetchContent_Declare(yy_thunks
     DOWNLOAD_EXTRACT_TIMESTAMP true
 )
 FetchContent_Declare(vc_ltl 
-    URL https://github.com/Chuyu-Team/VC-LTL5/releases/download/v5.0.9/VC-LTL-5.0.9-Binary.7z
+    URL https://github.com/Chuyu-Team/VC-LTL5/releases/download/v5.2.1/VC-LTL-Binary.7z
     DOWNLOAD_EXTRACT_TIMESTAMP true
 )
 
-if(USE_VC_LTL)
-    FetchContent_MakeAvailable(vc_ltl)
+if(WIN10ABOVE)
+    set(YY_Thunks )
+    add_library(VC_LTL INTERFACE)
+else()
+    option(VC_LTL_EnableCMakeInterface "VC_LTL_EnableCMakeInterface" ON)
+    FetchContent_MakeAvailable(vc_ltl yy_thunks)
     if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
         set(LTLPlatform "Win32")
     endif()
@@ -22,12 +25,7 @@ if(USE_VC_LTL)
         set(WindowsTargetPlatformMinVersion "5.1.2600.0")
     endif()
     include("${vc_ltl_SOURCE_DIR}/VC-LTL helper for cmake.cmake")
-endif()
 
-if(WIN10ABOVE)
-    set(YY_Thunks )
-else()
-    FetchContent_MakeAvailable(yy_thunks)
     if(WINXP)
         add_definitions(-DWINXP=${WINXP})
         set(YY_Thunks ${yy_thunks_SOURCE_DIR}/objs/X86/YY_Thunks_for_WinXP.obj)
