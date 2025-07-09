b5814936c8ac75f928e8c5c9bd041b54ac14943d
HIllya51 <1173718158@qq.com>
2025-07-09 22:10:28 +0800

    .

--- src/cpp/NativeUtils/needprotobuf/glens_1.cpp
+++ src/cpp/NativeUtils/needprotobuf/glens_1.cpp
@@ -7,44 +7,20 @@ using namespace lens;
 void _glens_create_request(uint64_t uuid, const char *analytics_id, size_t size1, const char *bytes, size_t size, int32_t width, int32_t height, void (*cb)(const char *, size_t))
 {
     LensOverlayServerRequest request;
-    auto objects_request = new LensOverlayObjectsRequest;
-    auto request_context = new LensOverlayRequestContext;
-    auto request_id = new LensOverlayRequestId;
-    auto routing_info = new LensOverlayRoutingInfo;
-    auto client_context = new LensOverlayClientContext;
-    auto locale_context = new LocaleContext;
-    auto client_filters = new AppliedFilters;
-    request_id->set_uuid(uuid);
-    request_id->set_sequence_id(0);
-    request_id->set_image_sequence_id(0);
-    request_id->set_analytics_id(analytics_id, size1);
-    request_id->set_allocated_routing_info(routing_info);
-    request_context->set_allocated_request_id(request_id);
+    request.mutable_objects_request()->mutable_request_context()->mutable_request_id()->set_uuid(uuid);
+    request.mutable_objects_request()->mutable_request_context()->mutable_request_id()->set_sequence_id(0);
+    request.mutable_objects_request()->mutable_request_context()->mutable_request_id()->set_image_sequence_id(0);
+    request.mutable_objects_request()->mutable_request_context()->mutable_request_id()->set_analytics_id(analytics_id, size1);
 
-    client_context->set_platform(Platform::PLATFORM_WEB);
-    client_context->set_surface(Surface::SURFACE_CHROMIUM);
-    locale_context->set_time_zone("");
-    client_context->set_allocated_locale_context(locale_context);
-    client_context->set_app_id("");
+    request.mutable_objects_request()->mutable_request_context()->mutable_client_context()->set_platform(Platform::PLATFORM_WEB);
+    request.mutable_objects_request()->mutable_request_context()->mutable_client_context()->set_surface(Surface::SURFACE_CHROMIUM);
+    request.mutable_objects_request()->mutable_request_context()->mutable_client_context()->mutable_locale_context()->set_time_zone("");
+    request.mutable_objects_request()->mutable_request_context()->mutable_client_context()->set_app_id("");
+    request.mutable_objects_request()->mutable_request_context()->mutable_client_context()->mutable_client_filters()->add_filter()->set_filter_type(LensOverlayFilterType::AUTO_FILTER);
 
-    auto filter = client_filters->add_filter();
-    filter->set_filter_type(LensOverlayFilterType::AUTO_FILTER);
-    client_context->set_allocated_client_filters(client_filters);
-
-    request_context->set_allocated_client_context(client_context);
-    objects_request->set_allocated_request_context(request_context);
-
-    auto image_data = new ImageData;
-    auto payload = new ImagePayload;
-    payload->set_image_bytes(bytes, size);
-    image_data->set_allocated_payload(payload);
-    auto image_metadata = new ImageMetadata;
-    image_metadata->set_width(width);
-    image_metadata->set_height(width);
-    image_data->set_allocated_image_metadata(image_metadata);
-    objects_request->set_allocated_image_data(image_data);
-
-    request.set_allocated_objects_request(objects_request);
+    request.mutable_objects_request()->mutable_image_data()->mutable_payload()->set_image_bytes(bytes, size);
+    request.mutable_objects_request()->mutable_image_data()->mutable_image_metadata()->set_width(width);
+    request.mutable_objects_request()->mutable_image_data()->mutable_image_metadata()->set_height(width);
 
     auto result = request.SerializeAsString();
     cb(result.c_str(), result.size());
