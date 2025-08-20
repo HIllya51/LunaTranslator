527f0b660cd09b3cc5272aebc2534e8c22e55eb8
chlbc <chris010613@yahoo.com.tw>
2025-08-20 19:43:04 +0800

    fix: cht.ts

--- docs/.vitepress/config/cht.ts
+++ docs/.vitepress/config/cht.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vitepress'
 //https://github.com/vuejs/vitepress/blob/main/docs/.vitepress/config/zh.ts#L161C2-L205C2
 export const chtSearch = {
     cht: {
-        placeholder: '搜索文檔',
+        placeholder: '搜尋文件',
         translations: {
             button: {
-                buttonText: '搜索文檔',
-                buttonAriaLabel: '搜索文檔'
+                buttonText: '搜尋文件',
+                buttonAriaLabel: '搜尋文件'
             },
             modal: {
                 searchBox: {
@@ -16,28 +16,28 @@ export const chtSearch = {
                     cancelButtonAriaLabel: '取消'
                 },
                 startScreen: {
-                    recentSearchesTitle: '搜索歷史',
-                    noRecentSearchesText: '沒有搜索歷史',
-                    saveRecentSearchButtonTitle: '保存至搜索歷史',
-                    removeRecentSearchButtonTitle: '從搜索歷史中移除',
-                    favoriteSearchesTitle: '收藏',
-                    removeFavoriteSearchButtonTitle: '從收藏中移除'
+                    recentSearchesTitle: '搜尋歷史',
+                    noRecentSearchesText: '沒有搜尋歷史',
+                    saveRecentSearchButtonTitle: '儲存至搜尋歷史',
+                    removeRecentSearchButtonTitle: '從搜尋歷史中移除',
+                    favoriteSearchesTitle: '我的最愛',
+                    removeFavoriteSearchButtonTitle: '從我的最愛中移除'
                 },
                 errorScreen: {
-                    titleText: '無法獲取結果',
-                    helpText: '你可能需要檢查你的網絡連接'
+                    titleText: '無法取得結果',
+                    helpText: '您可能需要檢查您的網路連接'
                 },
                 footer: {
                     selectText: '選擇',
                     navigateText: '切換',
                     closeText: '關閉',
-                    searchByText: '搜索提供者'
+                    searchByText: '搜尋提供者'
                 },
                 noResultsScreen: {
                     noResultsText: '無法找到相關結果',
-                    suggestedQueryText: '你可以嘗試查詢',
-                    reportMissingResultsText: '你認爲該查詢應該有結果？',
-                    reportMissingResultsLinkText: '點擊反饋'
+                    suggestedQueryText: '您可以嘗試查詢',
+                    reportMissingResultsText: '您認為該查詢應該有結果？',
+                    reportMissingResultsLinkText: '點擊回饋'
                 }
             }
         }
@@ -47,10 +47,10 @@ export const cht = defineConfig({
     themeConfig: {
         outline: {
             level: [2, 3],
-            label: "頁面導航"
+            label: "頁面導覽"
         },
         footer: {
-            copyright: `基於 <a href="https://github.com/HIllya51/LunaTranslator/blob/main/LICENSE">GPLv3</a> 許可發佈`
+            copyright: `基於 <a href="https://github.com/HIllya51/LunaTranslator/blob/main/LICENSE">GPLv3</a> 授權條款發佈`
         },
 
         editLink: {
@@ -72,8 +72,8 @@ export const cht = defineConfig({
         },
         nav: [
             // { text: "官方網站", link: "https://lunatranslator.org/" },
-            { text: "視頻教程", link: "https://space.bilibili.com/592120404/video" },
-            { text: "QQ羣", link: "https://qm.qq.com/q/I5rr3uEpi2" },
+            { text: "影片教學", link: "https://space.bilibili.com/592120404/video" },
+            { text: "QQ 群", link: "https://qm.qq.com/q/I5rr3uEpi2" },
             { text: "Discord", link: "https://discord.com/invite/ErtDwVeAbB" },
             { text: "支持作者", link: "/cht/support" },
         ],
@@ -81,7 +81,7 @@ export const cht = defineConfig({
             {
                 text: '基本的',
                 items: [
-                    { text: '下載 & 啟動 & 更新', link: '/cht/README' },
+                    { text: '下載 ＆ 啟動 ＆ 更新', link: '/cht/README' },
                     { text: '基本用法', link: '/cht/basicuse' },
                     { text: '支持作者', link: '/cht/support' }
                 ]
@@ -90,35 +90,35 @@ export const cht = defineConfig({
                 text: '詳細的',
                 items: [
                     {
-                        text: 'HOOK相關設置', link: '/cht/hooksettings',
+                        text: 'HOOK 相關設定', link: '/cht/hooksettings',
                         collapsed: true,
                         items: [
-                            { text: 'HOOK設置', link: '/cht/hooksettings' },
+                            { text: 'HOOK 設定', link: '/cht/hooksettings' },
                             { text: '內嵌翻譯', link: '/cht/embedtranslate' },
-                            { text: '模擬器遊戲支持', link: '/cht/emugames' },
+                            { text: '模擬器遊戲支援', link: '/cht/emugames' },
                         ]
                     },
                     {
-                        text: 'OCR相關設置', link: '/cht/useapis/ocrapi',
+                        text: 'OCR 相關設定', link: '/cht/useapis/ocrapi',
                         collapsed: true,
                         items: [
-                            { text: 'OCR接口設置', link: '/cht/useapis/ocrapi' },
-                            { text: 'OCR自動化執行方法', link: '/cht/ocrparam' },
+                            { text: 'OCR 介面設定', link: '/cht/useapis/ocrapi' },
+                            { text: 'OCR 自動化執行方法', link: '/cht/ocrparam' },
                         ]
                     },
                     {
-                        text: '翻譯接口設置', link: '/cht/useapis/tsapi',
+                        text: '翻譯介面設定', link: '/cht/useapis/tsapi',
                         collapsed: true,
                         items: [
-                            { text: '大模型翻譯接口', link: '/cht/guochandamoxing' },
-                            { text: '傳統在線翻譯接口', link: '/cht/useapis/tsapi' },
+                            { text: '大模型翻譯介面', link: '/cht/guochandamoxing' },
+                            { text: '傳統線上翻譯介面', link: '/cht/useapis/tsapi' },
                         ]
                     },
                     {
-                        text: '文本處理&翻譯優化', link: '/cht/textprocess',
+                        text: '文字處理＆翻譯優化', link: '/cht/textprocess',
                         collapsed: true,
                         items: [
-                            { text: '各種文本處理方法的作用和用法', link: '/cht/textprocess' },
+                            { text: '各種文字處理方法的作用和用法', link: '/cht/textprocess' },
                             { text: '各種翻譯優化的作用', link: '/cht/transoptimi' }
                         ]
                     },
@@ -136,21 +136,21 @@ export const cht = defineConfig({
                         items: [
                             { text: '日語分詞及假名注音', link: '/cht/qa1' },
                             { text: '使用內建查詞工具', link: '/cht/internaldict' },
-                            { text: '安裝Yomitan瀏覽器插件', link: '/cht/yomitan' },
-                            { text: 'Anki集成', link: '/cht/qa2' },
+                            { text: '安裝 Yomitan 瀏覽器外掛程式', link: '/cht/yomitan' },
+                            { text: 'Anki 整合', link: '/cht/qa2' },
                         ]
                     },
                     { text: '工具按鈕', link: '/cht/alltoolbuttons' },
-                    { text: '快捷按鍵', link: '/cht/fastkeys' },
-                    { text: '網絡服務', link: '/cht/apiservice' },
-                    { text: '語音識別', link: '/cht/sr' },
+                    { text: '快速按鍵', link: '/cht/fastkeys' },
+                    { text: '網路服務', link: '/cht/apiservice' },
+                    { text: '語音辨識', link: '/cht/sr' },
                     {
                         text: '實用技巧', link: '/cht/gooduse/multiconfigs',
                         collapsed: true,
                         items: [
-                            { text: '創建多份配置檔案', link: '/cht/gooduse/multiconfigs' },
-                            { text: '在HOOK模式下臨時使用OCR', link: '/cht/gooduse/useocrinhook' },
-                            { text: 'OCR模式綁定遊戲窗口', link: '/cht/gooduse/gooduseocr' },
+                            { text: '建立多份設定檔', link: '/cht/gooduse/multiconfigs' },
+                            { text: '在 HOOK 模式下臨時使用 OCR', link: '/cht/gooduse/useocrinhook' },
+                            { text: 'OCR 模式綁定遊戲視窗', link: '/cht/gooduse/gooduseocr' },
                         ]
                     }
                 ]
