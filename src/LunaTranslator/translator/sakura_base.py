aec1619b3f9ab5b88de12ed272380c05b442e223
HIllya51 <1173718158@qq.com>
2025-10-02 17:29:02 +0800

    .

--- src/LunaTranslator/translator/sakura_base.py
+++ src/LunaTranslator/translator/sakura_base.py
@@ -126,26 +126,18 @@ class TS(basetrans):
                 }
             ]
             __gptdict = self.make_gpt_dict_text(gpt_dict)
-            _pro = ""
-            if __gptdict:
-                __gptdict = (
-                    "参考以下术语表（可为空，格式为src->dst #备注）\n"
-                    + __gptdict
-                    + "\n"
-                )
-                _pro += "根据以上术语表的对应关系和备注，"
+            __gptdict = (
+                "参考以下术语表（可为空，格式为src->dst #备注）\n" + __gptdict + "\n\n"
+            )
             __msg = []
             self._gpt_common_parse_context_2(
                 __msg, self.context, contextnum, True, True
             )
-            if __msg:
-                __msg = "历史翻译：" + __msg[1]["content"] + "\n"
-                _pro += "结合历史剧情和上下文，"
+            __msg = "历史翻译：" + __msg[1]["content"] + "\n\n"
             content = (
-                (__msg if __msg else "")
+                __msg
                 + __gptdict
-                + _pro
-                + "将下面的文本从日文翻译成简体中文：\n"
+                + "根据以上术语表的对应关系和备注，结合历史剧情和上下文，将下面的文本从日文翻译成简体中文：\n"
                 + query
             )
             messages.append({"role": "user", "content": content})
