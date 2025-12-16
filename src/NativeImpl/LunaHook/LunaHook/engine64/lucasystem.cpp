fe84137cd4ac9d8572e13468a18f83135b2aa9a8
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2025-12-16 23:14:20 +0800

    .

--- src/NativeImpl/LunaHook/LunaHook/engine64/lucasystem.cpp
+++ src/NativeImpl/LunaHook/LunaHook/engine64/lucasystem.cpp
@@ -13,19 +13,22 @@ bool InsertIG64Hook2()
   const BYTE BYTES[] = {
       0xBA, 0x3F, 0xFF, 0x00, 0x00,
       XX, 0x8B, XX,
-      0xE8, XX2, 0x00, 0x00};
+      0xE8, XX2, XX, 0x00};
   bool ok = false;
   auto addrs = Util::SearchMemory(BYTES, sizeof(BYTES), PAGE_EXECUTE, processStartAddress, processStopAddress);
   std::set<uintptr_t> collect;
   for (auto addr : addrs)
   {
-    ConsoleOutput("%p", addr);
+    BYTE check = *(BYTE *)(addr + 5 + 3 + 3);
+    if (check != 0x00 &&
+        check != 0x01 // Summer Pockets REFLECTION BLUE Ver1.3.2.1
+    )
+      continue;
     const BYTE aligned[] = {0xCC, 0xCC};
     auto addr1 = reverseFindBytes(aligned, sizeof(aligned), addr - 0x10000, addr);
     //[240830][1150510][Key] LUNARiA -Virtualized Moonchild- 多国語版 Chinese-English-Japanese DL版 (files)
     const BYTE sig2[] = {0x48, 0x89, XX, XX, XX, 0x55, 0x56, 0x57, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57};
     auto addr2 = reverseFindBytes(sig2, sizeof(sig2), addr - 0x10000, addr);
-    ConsoleOutput("%p %p", addr1, addr2);
     addr = max(addr1, addr2);
     if (!addr)
       continue;
