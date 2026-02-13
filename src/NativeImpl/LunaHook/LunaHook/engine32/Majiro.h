01ba2475f4b3d4c21a177ca9dec302342db1eb8e
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2026-02-13 20:05:25 +0800

    .

--- src/NativeImpl/LunaHook/LunaHook/engine32/Majiro.h
+++ src/NativeImpl/LunaHook/LunaHook/engine32/Majiro.h
@@ -5,9 +5,14 @@ class Majiro : public ENGINE
 public:
     Majiro()
     {
-
-        check_by = CHECK_BY::FILE_ALL;
-        check_by_target = check_by_list{L"data*.arc", L"stream*.arc"};
+        check_by = CHECK_BY::CUSTOM;
+        is_engine_certain = false;
+        check_by_target = []()
+        {
+            return Util::CheckFile(L"data*.arc") &&
+                   (Util::CheckFile(L"stream*.arc") ||
+                    (Util::CheckFile(L"scenario*.arc") && Util::CheckFile(L"fastdata*.arc") && Util::CheckFile(L"data*.arc") && Util::CheckFile(L"voice*.arc")));
+        };
     };
     bool attach_function();
 };
