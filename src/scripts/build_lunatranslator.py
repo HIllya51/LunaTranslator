885d22bafc54168568fcb6c05d52c09344481e5e
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2026-03-19 22:49:45 +0800

    Move version info to version.txt and update build scripts

--- src/scripts/build_lunatranslator.py
+++ src/scripts/build_lunatranslator.py
@@ -289,10 +289,10 @@ if __name__ == "__main__":
     if sys.argv[1] == "download":
         downloadalls(sys.argv[2] if len(sys.argv) >= 3 else "")
     elif sys.argv[1] == "loadversion":
-        with open("version.cmake", "r", encoding="utf8") as ff:
-            pattern = r"set\(VERSION_MAJOR\s*(\d+)\s*\)\nset\(VERSION_MINOR\s*(\d+)\s*\)\nset\(VERSION_PATCH\s*(\d+)\s*\)\nset\(VERSION_REVISION\s*(\d+)\s*\)"
-            match = re.findall(pattern, ff.read())[0]
-            version_major, version_minor, version_patch, version_revison = match
+        with open("version.txt", "r", encoding="utf8") as ff:
+            version_major, version_minor, version_patch, version_revison = (
+                ff.read().strip().split(".")
+            )
             versionstring = f"v{version_major}.{version_minor}.{version_patch}"
             if int(version_revison):
                 versionstring += f".{version_revison}"
