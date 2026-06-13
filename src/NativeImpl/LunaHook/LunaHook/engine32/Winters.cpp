1a03336373357ca8870c5db99307622f90593295
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2026-06-13 12:48:03 +0800

    Add support for additional game version in Winters engine hook

--- src/NativeImpl/LunaHook/LunaHook/engine32/Winters.cpp
+++ src/NativeImpl/LunaHook/LunaHook/engine32/Winters.cpp
@@ -13,11 +13,29 @@ bool Winters::attach_function()
       0x83, 0xc1, 0x11,
       0xba, 0x19, 0x00, 0x00, 0x00,
       0xe8, XX4};
+  // ゴメンなさい・・・アタシのせいで
+  // https://vndb.org/v4267
+  const BYTE bytes2[] = {
+      0x4a,
+      0x8b, 0xda,
+      0xc1, 0xe3, 0x03,
+      0x8d, 0x1c, 0x5b,
+      0x8b, 0xcb,
+      0X41,
+      0X81, 0XC1, 0X70, 0X01, 0X00, 0X00,
+      0XBA, 0X27, 0X00, 0X00, 0X00,
+      0xe8, XX4};
+  auto off1 = sizeof(bytes);
   ULONG addr = MemDbg::findBytes(bytes, sizeof(bytes), processStartAddress, processStopAddress);
   if (!addr)
+  {
+    off1 = sizeof(bytes2);
+    addr = MemDbg::findBytes(bytes2, sizeof(bytes2), processStartAddress, processStopAddress);
+  }
+  if (!addr)
     return false;
-  auto off = *(DWORD *)(addr + sizeof(bytes) - 4);
-  auto target = off + addr + sizeof(bytes);
+  auto off = *(DWORD *)(addr + off1 - 4);
+  auto target = off + addr + off1;
   HookParam hp;
   hp.address = target;
   hp.offset = stackoffset(1);
--- src/NativeImpl/LunaHook/LunaHook/engine32/Winters.h
+++ src/NativeImpl/LunaHook/LunaHook/engine32/Winters.h
@@ -6,7 +6,7 @@ public:
     Winters()
     {
         check_by = CHECK_BY::FILE_ALL;
-        check_by_target = check_by_list{L"MSG.dat", L"SCR.ifx", L"VFN.dat", L"GRP.ifx"};
+        check_by_target = check_by_list{L"MSG.dat", L"SCR.ifx", L"GRP.ifx"};
     };
     bool attach_function();
 };
